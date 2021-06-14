a93c2e89018c7addd13d7b5b4b5a3f7d2c20eb99
Subio <subikos@mail.ru>
2021-06-14 10:45:32 +0500

    accepted acmp 313

--- acmp/task-0313/code.cpp
+++ acmp/task-0313/code.cpp
@@ -5,7 +5,27 @@
 // Space complexity: O(n)
 
 int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
 	int n;
-	scanf("%d", n);
+	scanf("%d", &n);
+	std::vector<int> bus(n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &bus[i]);
+	}
+	int maxTime = 0;
+	for (int i = 0; i < n; i++) {
+		int tempTime = 0;
+		for (int j = i + 1; j < n; j++) {
+			tempTime++;
+			if (bus[j] == bus[i]) {
+				if (tempTime > maxTime) {
+					maxTime = tempTime;
+				}
+				break;
+			}
+		}
+	}
+	printf("%d", maxTime);
 	return 0;
 }
