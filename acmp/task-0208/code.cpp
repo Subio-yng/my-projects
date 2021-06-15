0a343fe35c09add57686e7c93dba857fff4f1020
Subio <subikos@mail.ru>
2021-06-15 15:37:50 +0500

    improved acmp 208

--- acmp/task-0208/code.cpp
+++ acmp/task-0208/code.cpp
@@ -5,33 +5,38 @@
 // Time complexity: O(len(str)^2)
 // Space complexity: O(1)
 
+std::string convertToBinaryString(int n) {
+	std::string a;
+	while (n > 0) {
+		a += n % 2 + '0';
+		n >>= 1;
+	}
+	for (int i = 0, j = (int) a.length() - 1; i < j; i++, j--) {
+		std::swap(a[i], a[j]);
+	}
+	return a;
+}
+
+void cyclicShiftLeft(std::string &a) {
+	std::string temp = a.substr(0, (int) a.length() - 1);
+	a = a.back();
+	a += temp;
+}
+
 int main() {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 	int n;
 	scanf("%d", &n);
-	std::string str;
+	std::string str = convertToBinaryString(n);
 	int max = n;
-	while (n > 0) {
-		str += n % 2 + '0';
-		n /= 2;
-	}
-	for (int i = 0, j = (int) str.length() - 1; i < j; i++, j--) {
-		std::swap(str[i], str[j]);
-	}
 	for (int i = 0; i < (int) str.length(); i++) {
-		char back = str.back();
-		for (int j = (int) str.length() - 1; j > 0; j--) {
-			str[j] = str[j - 1];
-		}
-		str[0] = back;
+		cyclicShiftLeft(str);
 		int temp = 0;
 		for (int i = 0; i < (int) str.length(); i++) {
 			temp += (str[i] - '0') * (1 << (int) str.length() - i - 1);
 		}
-		if (temp > max) {
-			max = temp;
-		}
+		max = std::max(max, temp);
 	}
 	printf("%d", max);
 	return 0;
