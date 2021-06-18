10b29adb39079e33fe9d8330d7f13e61cef2055e
Subio <subikos@mail.ru>
2021-06-18 17:47:21 +0500

    accepted acmp 779

---
+++ acmp/task-0779/code.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	int n;
+	scanf("%d", &n);
+	int mid = n >> 1, ans = 0;
+	for (int i = 0; i < n; i++) {
+		int house;
+		scanf("%d", &house);
+		if (i == mid) {
+			ans = house;
+		}
+	}
+	printf("%d", ans);
+	return 0;
+}
