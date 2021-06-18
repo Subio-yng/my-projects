c05f202c0a646b9c95047592d9dd4eb98986279a
Subio <subikos@mail.ru>
2021-06-18 18:05:43 +0500

    acmp 779 fix code-style

---
+++ acmp/task-0779/code.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int mid = n >> 1;
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        int house;
+        scanf("%d", &house);
+        if (i == mid) {
+            ans = house;
+        }
+    }
+    printf("%d", ans);
+    return 0;
+}
