0571c45211df0218048cf03d8c922637ba93c111
Subio <subikos@mail.ru>
2021-06-19 16:59:29 +0500

    updated acmp 70

--- acmp/task-0070/code.cpp
+++ acmp/task-0070/code.cpp
@@ -1,50 +1,38 @@
 #include <stdio.h>
+#include <algorithm>
 #include <string>
 
-// Time complexity: O(n + len(str))
+// Time complexity: O(n * len(str))
 // Space complexity: O(buf)
 
-const int MAXSIZE = 1023;
-
-void powerString(std::string &a, int n) {
-	std::string temp = a;
-	for (int i = 1; i < n; i++) {
-		if ((int) temp.length() * (i + 1) >= MAXSIZE) {
-			for (int j = 0; (int) temp.length() * i + j < MAXSIZE; j++) {
-				a += temp[j];
-			}
-			break;
-		}
-		a += temp;
-	}
-}
-
 int main() {
-	freopen("intput.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-	char buf[1 + 1000];
-	scanf("%s", &buf);
-	std::string str(buf);
-	int n;
-	scanf("%d", &n);
-	if (n > 0) {
-		powerString(str, n);
-		printf("%s", str.c_str());
-	} else {
-		std::string root = str;
-		for (int i = -1; i > n; i--) {
-			if ((int) root.length() == 1) {
-				break;
-			}
-			root = root.substr(0, (int) root.length() >> 1);
-		}
-		std::string temp = root;
-		powerString(temp, n * -1);
-		if (temp == str) {
-			printf("%s", root.c_str());
-		} else {
-			printf("NO SOLUTION");
-		}
-	}
-	return 0;
+    freopen("intput.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    const int MAXSIZE = 1023;
+    char buf[1 + 1000];
+    scanf("%s", &buf);
+    std::string str(buf);
+    int n;
+    scanf("%d", &n);
+    if (n > 0) {
+        for (int i = 0; i < std::min(n * (int) str.length(), MAXSIZE); i++) {
+            printf("%c", str[i % (int) str.length()]);
+        }
+    } else {
+        n = n * -1 - 1;
+        std::string root;
+        for (int i = 0; i < (int) str.length() >> n; i++) {
+            root += str[i];
+        }
+        std::string temp;
+        for (int i = 0; i <= n; i++) {
+            temp += root;
+        }
+        if (temp == str) {
+            printf("%s", root.c_str());
+        } else {
+            printf("NO SOLUTION");
+        }
+    }
+    return 0;
 }
