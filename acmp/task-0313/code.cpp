69f5ea4ac7df9ca4b0dc315b386d0102fa354dc4
Subio <subikos@mail.ru>
2021-06-20 16:11:43 +0500

    updated acmp 313

---
+++ acmp/task-0313/code.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <vector>
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	int n;
+	scanf("%d", &n);
+	std::vector<int> route(100, -1);
+	int maxTime = 0;
+	for (int i = 0; i < n; i++) {
+		int bus;
+		scanf("%d", &bus);
+		if (route[bus - 1] != -1 && i - route[bus - 1] > maxTime) {
+			maxTime = i - route[bus - 1];
+		}
+		route[bus - 1] = i;
+	}
+	printf("%d", maxTime);
+	return 0;
+}
