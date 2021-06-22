e230801b71525a027d883edcbfe0323ded30fa65
Subio <subikos@mail.ru>
2021-06-22 12:28:04 +0500

    accepted acmp 793

---
+++ acmp/task-0793/code.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+// Time complexity: O(n * log(n) * sqrt(n))
+// Space complexity: O(1)
+
+bool isPrime(int n) {
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return n > 1;
+}
+
+int sumNum(int n) {
+    int sum = 0;
+    for (int i = 1; i <= n; i *= 10) {
+        sum += n / i % 10;
+    }
+    return sum;
+}
+
+int sumPrime(int n) {
+    int sum = 0, temp = n;
+    int dev = 2;
+    while (n > 1) {
+        if (dev == temp) {
+            break;
+        }
+        if (isPrime(dev) && n % dev == 0) {
+            n /= dev;
+            sum += sumNum(dev);
+        } else {
+            dev++;
+        }
+    }
+    return sum;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    while (true) {
+        {
+            int num;
+            int code = scanf("%d", &num);
+            if (code == EOF) {
+                break;
+            }
+            if (sumNum(num) == sumPrime(num)) {
+                printf("1");
+            } else {
+                printf("0");
+            }
+        }
+    }
+    return 0;
+}
