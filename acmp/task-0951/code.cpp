c8dbac2eca22565a8319874845accf965e2397aa
Subio <subikos@mail.ru>
2021-06-22 15:19:02 +0500

    accepted acmp 951

--- acmp/task-0951/code.cpp
+++ acmp/task-0951/code.cpp
@@ -7,34 +7,36 @@
 
 struct Coord {
 
-    int x;
+	int x;
 
-    int y;
+	int y;
 };
 
 int main() {
-    int n, m;
-    scanf("%d %d", &n, &m);
-    int virus;
-    scanf("%d", &virus);
-    std::vector<Coord> vec(virus);
-    for (int i = 0; i < virus; i++) {
-        scanf("%d %d", &vec[i].x, &vec[i].y);
-    }
-    int maxSeconds = 0;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            int tempSeconds = 0;
-            for (int k = 0; k < virus; k++) {
-                if (k == 0 || abs(vec[k].x - i) + abs(vec[k].y - j) < tempSeconds) {
-                    tempSeconds = abs(vec[k].x - i) + abs(vec[k].y - j);
-                }
-            }
-            if (tempSeconds > maxSeconds) {
-                maxSeconds = tempSeconds;
-            }
-        }
-    }
-    printf("%d", maxSeconds);
-    return 0;
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	int n, m;
+	scanf("%d %d", &n, &m);
+	int virus;
+	scanf("%d", &virus);
+	std::vector<Coord> vec(virus);
+	for (int i = 0; i < virus; i++) {
+		scanf("%d %d", &vec[i].x, &vec[i].y);
+	}
+	int maxSeconds = 0;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			int tempSeconds = 0;
+			for (int k = 0; k < virus; k++) {
+				if (k == 0 || abs(vec[k].x - i) + abs(vec[k].y - j) < tempSeconds) {
+					tempSeconds = abs(vec[k].x - i) + abs(vec[k].y - j);
+				}
+			}
+			if (tempSeconds > maxSeconds) {
+				maxSeconds = tempSeconds;
+			}
+		}
+	}
+	printf("%d", maxSeconds);
+	return 0;
 }
