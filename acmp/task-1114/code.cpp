14f6f87c4f2c8a59e2d4d0dea17354d3bee7f1a1
Subio <subikos@mail.ru>
2021-06-24 19:13:44 +0500

    updated acmp 1114

--- acmp/task-1114/code.cpp
+++ acmp/task-1114/code.cpp
@@ -7,9 +7,9 @@ int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     const int DISTANCE = 109;
-    int speed, time;
-    scanf("%d %d", &speed, &time);
-    int ans = speed * time % DISTANCE + 1;
+    int speed, hours;
+    scanf("%d %d", &speed, &hours);
+    int ans = speed * hours % DISTANCE + 1;
     if (speed < 0) {
         ans += DISTANCE;
     }
