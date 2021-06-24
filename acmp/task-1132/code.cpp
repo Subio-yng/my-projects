f64245a168bcfb7869fc521d7919ea6b3f7a8805
Subio <subikos@mail.ru>
2021-06-24 19:09:48 +0500

    updated acmp 1132

--- acmp/task-1132/code.cpp
+++ acmp/task-1132/code.cpp
@@ -4,15 +4,12 @@
 // Space complexity: O(1)
 
 int main() {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    int length = 0;
+    int length = -1;
     int n;
-    scanf("%d", &n);
-    while (n != 0) {
-        length++;
+    do {
         scanf("%d", &n);
-    }
+        length++;
+    } while (n != 0);
     printf("%d", length);
     return 0;
 }
