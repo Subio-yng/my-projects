7ea611a506ab9883135101327aa5362dd931c4de
Subio <subikos@mail.ru>
2021-06-24 21:47:30 +0500

    updated acmp 1118

--- acmp/task-1118/code.cpp
+++ acmp/task-1118/code.cpp
@@ -3,10 +3,7 @@
 // Time complexity: O(1)
 // Space complexity: O(1)
 
-int div(int a, int b) {
-    if (a < 0) {
-        return 0;
-    }
+int divCeil(int a, int b) {
     return (a + b - 1) / b;
 }
 
@@ -15,6 +12,10 @@ int main() {
     freopen("output.txt", "w", stdout);
     int height, day, night;
     scanf("%d %d %d", &height, &day, &night);
-    printf("%d", div((height - day), (day - night)) + 1);
+    if (day > height) {
+        printf("1");
+    } else {
+        printf("%d", divCeil(height - day, day - night) + 1);
+    }
     return 0;
 }
