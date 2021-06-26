77d4874a871ef0a4f5dee2161a91f55fffbbc1e2
Subio <subikos@mail.ru>
2021-06-26 20:35:05 +0500

    accepted acmp 1143

---
+++ acmp/task-1143/code.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// Time comeplexity: O(n)
+// Space complexity: O(1)
+
+int main() {
+    int prev;
+    scanf("%d", &prev);
+    if (prev == 0) {
+        printf("0");
+        return 0;
+    }
+    int cur;
+    scanf("%d", &cur);
+    if (cur == 0) {
+        printf("0");
+        return 0;
+    }
+    int next = -1;
+    int locMaxCount = 0;
+    do {
+        if (next != -1) {
+            prev = cur;
+            cur = next;
+        }
+        scanf("%d", &next);
+        if (next != 0 && cur > prev && cur > next) {
+            locMaxCount++;
+        }
+    } while (next != 0);
+    printf("%d", locMaxCount);
+    return 0;
+}
