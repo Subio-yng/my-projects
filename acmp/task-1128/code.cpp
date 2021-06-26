95be1f966719a1f9aac29b2063ba037449e6cc5e
Subio <subikos@mail.ru>
2021-06-26 22:26:18 +0500

    acmp 1128 fix code-style

--- acmp/task-1128/code.cpp
+++ acmp/task-1128/code.cpp
@@ -5,12 +5,11 @@
 
 int main() {
     const int PERCENT = 15;
+    const double EPS = 1e-6;
     double distance1, distance2;
     scanf("%lf %lf", &distance1, &distance2);
-    distance1 *= 1000;
-    distance2 *= 1000;
     int day = 1;
-    while (distance1 < distance2) {
+    while (distance1 + EPS < distance2) {
         distance1 += distance1 * PERCENT / 100;
         day++;
     }
