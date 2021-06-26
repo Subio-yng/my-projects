c5a8a5db39b7ef658df4bde2e782adadc03bf683
Subio <subikos@mail.ru>
2021-06-26 16:11:26 +0500

    acmp 1088 improve moveQueen

--- acmp/task-1088/code.cpp
+++ acmp/task-1088/code.cpp
@@ -15,7 +15,7 @@ bool moveBishop(int x1, int y1, int x2, int y2) {
 }
 
 bool moveQueen(int x1, int y1, int x2, int y2) {
-    return x1 == x2 || y1 == y2 || abs(x1 - x2) == abs(y1 - y2);
+    return moveRook(x1, y1, x2, y2) || moveBishop(x1, y1, x2, y2);
 }
 
 bool moveKnight(int x1, int y1, int x2, int y2) {
@@ -27,8 +27,8 @@ bool moveKing(int x1, int y1, int x2, int y2) {
 }
 
 bool movePawn(int x1, int y1, int x2, int y2) {
-    return (x1 == x2 && (y2 - y1 == 1 || y2 - y1 == 2) && y1 > '1') &&
-        !(x1 != 2 && x2 - x1 == 2);
+    return y1 == y2 && (x2 - x1 == 1 || x2 - x1 == 2) && x1 > 1 &&
+        !(x1 != 2 && x2 - x1 == 2;
 }
 
 int main() {
