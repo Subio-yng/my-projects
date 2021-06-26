21da46981790b3de9e8ced41888ed6e9d7e014e8
Subio <subikos@mail.ru>
2021-06-26 16:04:09 +0500

    acmp 1479 fix code-style

--- acmp/task-1479/code.cpp
+++ acmp/task-1479/code.cpp
@@ -1,18 +1,13 @@
 #include <stdio.h>
+#include <algorithm>
 
 // Time complexity: O(1)
 // Space complexity: O(1)
 
-long long abs(long long a) {
-    if (a < 0) {
-        return a * -1;
-    }
-    return a;
-}
-
 int main() {
     long long n, div;
     scanf("%lld %lld", &n, &div);
-    printf("%lld", (n % div + abs(div)) % div);
+    div = std::llabs(div);
+    printf("%lld", (n % div + div) % div);
     return 0;
 }
