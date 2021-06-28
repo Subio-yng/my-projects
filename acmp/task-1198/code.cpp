93d84b20b79a6f7c49c27538eadf40e32aa969c2
Subio <subikos@mail.ru>
2021-06-28 15:42:43 +0500

    acmp 1198 fix code-style

--- acmp/task-1198/code.cpp
+++ acmp/task-1198/code.cpp
@@ -4,22 +4,24 @@
 // Space complexity: O(1)
 
 int main() {
+    const int MODLETTERS = 26;
+    const int MODNUMBERS = 10;
     int length;
     scanf("%d", &length);
-    int capital, lowercase, number;
-    scanf("%d %d %d", &capital, &lowercase, &number);
+    int uppercase, lowercase, number;
+    scanf("%d %d %d", &uppercase, &lowercase, &number);
     for (int i = 0; i < length; i++) {
-        if (capital > 0) {
-            printf("%c", 'A' + i % 26);
-            capital--;
+        if (uppercase > 0) {
+            printf("%c", 'A' + i % MODLETTERS);
+            uppercase--;
         } else if (lowercase > 0) {
-            printf("%c", 'a' + i % 26);
+            printf("%c", 'a' + i % MODLETTERS);
             lowercase--;
         } else if (number > 0) {
-            printf("%c", '0' + i % 10);
+            printf("%c", '0' + i % MODNUMBERS);
             number--;
         } else {
-            printf("%c", 'A' + i % 26);
+            printf("%c", 'A' + i % MODLETTERS);
         }
     }
     return 0;
