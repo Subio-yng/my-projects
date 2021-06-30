f5eb9d7fd547b3bd89b8108e32a09cf1966c1e42
Subio <subikos@mail.ru>
2021-06-30 21:06:15 +0500

    updated acmp 1154

--- acmp/task-1154/code.cpp
+++ acmp/task-1154/code.cpp
@@ -3,18 +3,21 @@
 // Time complexity: O(1)
 // Space complexity: O(1)
 
+const int MAXIP = 255;
+const int MINIP = 0;
+
+bool isOk(long long n) {
+    return MINIP <= n && n <= MAXIP;
+}
+
 int main() {
-    const int MAXIP = 255;
-    const int MINIP = 0;
     long long n1 = -1;
     long long n2 = -1;
     long long n3 = -1;
     long long n4 = -1;
     scanf("%lld.%lld.%lld.%lld", &n1, &n2, &n3, &n4);
-    if (MINIP <= n1 && n1 <= MAXIP &&
-        MINIP <= n2 && n2 <= MAXIP &&
-        MINIP <= n3 && n3 <= MAXIP &&
-        MINIP <= n4 && n4 <= MAXIP
+    if (isOk(n1) && isOk(n2) && 
+        isOk(n3) && isOk(n4)
     ) {
         printf("Good");
     } else {
