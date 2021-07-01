b6221b23c284e0be26f92ffef8453ab2fb09bef5
Subio <subikos@mail.ru>
2021-07-01 12:58:58 +0500

    accepted acmp 1215

---
+++ acmp/task-1215/code.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+#include <vector>
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    std::vector<int> list(n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &list[i]);
+    }
+    int x;
+    scanf("%d", &x);
+    int ans = list[0];
+    for (int i = 1; i < n; i++) {
+        if (abs(x - list[i]) < abs(x - ans)) {
+            ans = list[i];
+        } else if (abs(x - list[i]) == abs(x - ans) && list[i] < ans) {
+            ans = list[i];
+        }
+    }
+    printf("%d", ans);
+    return 0;
+}
