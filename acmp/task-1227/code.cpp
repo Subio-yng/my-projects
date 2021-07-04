ceac0e308c0b035795112db270b82449aed52954
Subio <subikos@mail.ru>
2021-07-04 17:08:15 +0500

    acmp 1227 fix code-style

--- acmp/task-1227/code.cpp
+++ acmp/task-1227/code.cpp
@@ -3,16 +3,20 @@
 // Time complexity: O(1)
 // Space complexity: O(1)
 
-long long F(int n) {
+long long getF(int n) {
 	if (n <= 1) {
 		return 1;
 	}
 	return n * F(n - 1);
 }
 
+long long getC(int n, int k) {
+	return F(n) / (F(k) * F(n - k));
+}
+
 int main() {
 	int n, k;
 	scanf("%d %d", &n, &k);
-	printf("%lld", F(n) / (F(k) * F(n - k)));
+	printf("%lld", getC(n, k));
 	return 0;
 }
