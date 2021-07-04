b2a9f7c8dff760883f38d464be43147252bd325b
Subio <subikos@mail.ru>
2021-07-04 17:10:31 +0500

    acmp 1226 fix code-style

--- acmp/task-1226/code.cpp
+++ acmp/task-1226/code.cpp
@@ -3,7 +3,7 @@
 // Time complexity: O(1)
 // Space complexity: O(1)
 
-int IsLetter(int n) {
+int isLetter(int n) {
 	if ('a' <= n && n <= 'z' ||
 		'A' <= n && n <= 'Z'
 	) {
@@ -15,6 +15,6 @@ int IsLetter(int n) {
 int main() {
 	char n1, n2, n3;
 	scanf("%c %c %c", &n1, &n2, &n3);
-	printf("%d", IsLetter(n1) + IsLetter(n2) + IsLetter(n3));
+	printf("%d", isLetter(n1) + isLetter(n2) + isLetter(n3));
 	return 0;
 }
