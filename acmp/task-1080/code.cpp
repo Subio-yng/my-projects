bc6b09405515c3bda635ac31f291d4be9120427f
Subio <subikos@mail.ru>
2021-07-05 10:12:07 +0500

    updated acmp 1080

--- acmp/task-1080/code.cpp
+++ acmp/task-1080/code.cpp
@@ -5,12 +5,52 @@
 // Time complexity: O(n * m)
 // Space complexity: O(n)
 
+struct Array {
+
+	std::vector<int> vec;
+
+	int get(int id) {
+		return vec[id];
+	}
+
+	void update(int left, int right, int val) {
+		for (int i = left; i <= right; i++) {
+			vec[i] = val;
+		}
+	}
+
+	void add(int left, int right, int val) {
+		for (int i = left; i <= right; i++) {
+			vec[i] += val;
+		}
+	}
+
+	int rsq(int left, int right) {
+		int sum = 0;
+		for (int i = left; i <= right; i++) {
+			sum += vec[i];
+		}
+		return sum;
+	}
+
+	int rmq(int left, int right) {
+		int min = vec[left];
+		for (int i = left; i <= right; i++) {
+			if (min > vec[i]) {
+				min = vec[i];
+			}
+		}
+		return min;
+	}
+};
+
 int main() {
 	int n;
 	scanf("%d", &n);
-	std::vector<int> list(n);
+	Array list;
+	list.vec.resize(n);
 	for (int i = 0; i < n; i++) {
-		scanf("%d", &list[i]);
+		scanf("%d", &list.vec[i]);
 	}
 	int operations;
 	scanf("%d", &operations);
@@ -22,49 +62,35 @@ int main() {
 			int id;
 			scanf("%d", &id);
 			id--;
-			printf("%d\n", list[id]);
+			printf("%d\n", list.get(id));
 		}
 		if (inquiry == "update") {
 			int left, right, val;
 			scanf("%d %d %d", &left, &right, &val);
 			left--;
 			right--;
-			for (int j = left; j <= right; j++) {
-				list[j] = val;
-			}
+			list.update(left, right, val);
 		}
 		if (inquiry == "add") {
 			int left, right, val;
 			scanf("%d %d %d", &left, &right, &val);
 			left--;
 			right--;
-			for (int j = left; j <= right; j++) {
-				list[j] += val;
-			}
+			list.add(left, right, val);
 		}
 		if (inquiry == "rsq") {
 			int left, right;
 			scanf("%d %d", &left, &right);
 			left--;
 			right--;
-			int sum = 0;
-			for (int j = left; j <= right; j++) {
-				sum += list[j];
-			}
-			printf("%d\n", sum);
+			printf("%d\n", list.rsq(left, right));
 		}
 		if (inquiry == "rmq") {
 			int left, right;
 			scanf("%d %d", &left, &right);
 			left--;
 			right--;
-			int min = list[left];
-			for (int j = left; j <= right; j++) {
-				if (min > list[j]) {
-					min = list[j];
-				}
-			}
-			printf("%d\n", min);
+			printf("%d\n", list.rmq(left, right));
 		}
 	}
 	return 0;
