57ef0316173e79a4bbd24edac292049397da6d2c
Subio <subikos@mail.ru>
2021-07-06 08:56:59 +0500

    accepted acmp 1237

---
+++ acmp/task-1237/code.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <vector>
+
+// Time complexity: O(n * m)
+// Space complexity: O(n * m)
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    std::vector<std::vector<int>> list(n, std::vector<int>(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &list[i][j]);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            int val;
+            scanf("%d", &val);
+            printf("%d ", list[i][j] + val);
+        }
+        printf("\n");
+    }
+    return 0;
+}
