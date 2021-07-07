65c6642449bba8e2d679b5d53f9a25c68088c46d
Subio <subikos@mail.ru>
2021-07-07 15:53:42 +0500

    updated acmp 877

--- acmp/task-0877/code.cpp
+++ acmp/task-0877/code.cpp
@@ -6,31 +6,30 @@
 
 int main() {
     const int N_USERS = 2;
-    const std::string name1 = "Fedya";
+    const std::string NAME1 = "Fedya";
     char buf[1 + 250];
     scanf("%*d:%*d:%*d: %s signed on", &buf);
     std::string name2(buf);
+    std::string terminatePhrase = name2 + " signed off";
     int curUser = 0;
-    bool first = true;
-    while (first) {
+    while (true) {
         scanf("%*d:%*d:%*d: %[^\n]s", &buf);
-        std::string terminatePhrase(buf);
-        if (terminatePhrase == name2 + " signed off") {
-            first = !first;
-        } else {
-            if (terminatePhrase.back() == '.') {
-                terminatePhrase.back() = ',';
-            } else if (terminatePhrase.back() != '!' && terminatePhrase.back() != '?') {
-                terminatePhrase += ',';
-            }
-            printf("\"%s\" --- skazal ", terminatePhrase.c_str());
-            if (curUser % 2 == 0) {
-                printf("%s.\n", name1.c_str());
-            } else {
-                printf("%s.\n", name2.c_str());
-            }
-            curUser = N_USERS - 1 - curUser;
+        std::string phrase(buf);
+        if (phrase == terminatePhrase) {
+            break;
         }
+        if (phrase.back() == '.') {
+            phrase.back() = ',';
+        } else if (phrase.back() != '!' && phrase.back() != '?') {
+            phrase += ',';
+        }
+        printf("\"%s\" --- skazal ", phrase.c_str());
+        if (curUser % 2 == 0) {
+            printf("%s.\n", NAME1.c_str());
+        } else {
+            printf("%s.\n", name2.c_str());
+        }
+        curUser = N_USERS - 1 - curUser;
     }
     return 0;
 }
