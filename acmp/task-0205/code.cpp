06635f5be4c399834b2a9c592f61d20d221e37b0
Subio <subikos@mail.ru>
2021-07-20 10:41:06 +0500

    updated acmp 205

--- acmp/task-0205/code.cpp
+++ acmp/task-0205/code.cpp
@@ -6,33 +6,24 @@
 int main() {
     int hours, minutes, seconds;
     scanf("%d:%d:%d", &hours, &minutes, &seconds);
-    int val1;
-    scanf("%d", &val1);
-    char next;
-    int code = scanf("%c", &next);
+    int val1, val2 = -1, val3 = -1;
     int days = 0;
-    if (code != EOF && next != '\n') {
-        int val2;
-        scanf("%d", &val2);
-        code = scanf("%c", &next);
-        if (code != EOF && next != '\n') {
-            int val3;
-            scanf("%d", &val3);
-            seconds += val3 % 60;
-            minutes += seconds / 60 + val2 % 60 + val3 / 60 % 60;
-            hours += minutes / 60 + val1 % 24 + val2 / 60 % 24 + val3 / 3600 % 24;
-            days += val1 / 24 + val2 / 1440 + val3 / 86400;
-        } else {
-            seconds += val2 % 60;
-            minutes += seconds / 60 + val1 % 60 + val2 / 60 % 60;
-            hours += minutes / 60 + val1 / 60 % 24 + val2 / 3600 % 24;
-            days += val1 / 1440 + val2 / 86400;
-        }
-    } else {
+    scanf("%d:%d:%d", &val1, &val2, &val3);
+    if (val2 == -1 && val3 == -1) {
         seconds += val1 % 60;
         minutes += seconds / 60 + val1 / 60 % 60;
         hours += minutes / 60 + val1 / 3600 % 24;
         days += val1 / 86400;
+    } else if (val3 == -1) {
+        seconds += val2 % 60;
+        minutes += seconds / 60 + val1 % 60 + val2 / 60 % 60;
+        hours += minutes / 60 + val1 / 60 % 24 + val2 / 3600 % 24;
+        days += val1 / 1440 + val2 / 86400;
+    } else {
+        seconds += val3 % 60;
+        minutes += seconds / 60 + val2 % 60 + val3 / 60 % 60;
+        hours += minutes / 60 + val1 % 24 + val2 / 60 % 24 + val3 / 3600 % 24;
+        days += val1 / 24 + val2 / 1440 + val3 / 86400;
     }
     days += hours / 24;
     hours %= 24;
