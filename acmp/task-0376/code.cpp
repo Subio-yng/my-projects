a8c9f7c28ca54862d843522da537b15ab74b852a
Subio <subikos@mail.ru>
2021-07-27 18:35:42 +0500

    fix code-style acmp 376

--- acmp/task-0376/code.cpp
+++ acmp/task-0376/code.cpp
@@ -11,35 +11,31 @@ int main() {
     int curYear;
     scanf("%d", &curYear);
     int days = 0;
-    int daysInFebruary;
-    if (curYear % 4 == 0 && curYear % 100 != 0 || curYear % 400 == 0) {
-        daysInFebruary = 29;
-    } else {
-        daysInFebruary = 28;
-    }
     while (curDay != endDay || curMonth != endMonth) {
+        int daysInFebruary;
+        if (curYear % 4 == 0 && curYear % 100 != 0 || curYear % 400 == 0) {
+            daysInFebruary = 29;
+        } else {
+            daysInFebruary = 28;
+        }
         int daysInMonth;
         if (curMonth == 2) {
             daysInMonth = daysInFebruary;
         } else if (curMonth == 4 || curMonth == 6 ||
                    curMonth == 9 || curMonth == 11
-                   ) {
+        ) {
             daysInMonth = 30;
         } else {
             daysInMonth = 31;
         }
+        if (curMonth == 12 && curDay == daysInMonth) {
+            curMonth = 1;
+            curDay = 0;
+            curYear++;
+        }
         if (curDay == daysInMonth) {
             curDay = 0;
             curMonth++;
-        }
-        if (curMonth == 13) {
-            curMonth = 1;
-            curYear++;
-            if (curYear % 4 == 0 && curYear % 100 != 0 || curYear % 400 == 0) {
-                daysInFebruary = 29;
-            } else {
-                daysInFebruary = 28;
-            }
         }
         curDay++;
         days++;
