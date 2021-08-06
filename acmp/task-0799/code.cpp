239e26ef0505ca4ab804f7266b051e84b54754fd
Subio <subikos@mail.ru>
2021-08-06 12:17:11 +0500

    accepted acmp 799

---
+++ acmp/task-0799/code.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+// Time complexity: O(n * log(n))
+// Space complexity: O(n)
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    std::vector<int> list(n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &list[i]);
+    }
+    int maxScoreId = 0;
+    int score = 0;
+    for (int i = 1; i < n - 1; i++) {
+        if (list[i] % 10 == 5 && list[i + 1] < list[i] &&
+            maxScoreId < i && list[i] > score
+        ) {
+            score = list[i];
+        }
+        if (list[i] > list[maxScoreId]) {
+            maxScoreId = i;
+            score = 0;
+        }
+    }
+    if (score == 0) {
+        printf("0");
+    } else {
+        std::sort(list.rbegin(), list.rend());
+        for (int i = 0; i < n; i++) {
+            if (list[i] == score) {
+                printf("%d", i + 1);
+                break;
+            }
+        }
+    }
+    return 0;
+}
