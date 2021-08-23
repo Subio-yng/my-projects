6a2b9efc3613cdae614eddba07c39e588cd69f32
Subio <subikos@mail.ru>
2021-08-23 08:37:26 +0500

    updated acmp 1160

--- acmp/task-1160/code.cpp
+++ acmp/task-1160/code.cpp
@@ -5,26 +5,27 @@
 // Time complexity: O(len)
 // Space complexity: O(len)
 
+std::vector<int> getPrefixFunction(std::string sample) {
+    std::vector<int> p((int) sample.size(), 0);
+    for (int i = 1; i < sample.length(); i++) {
+        int prev = p[i - 1];
+        while (prev > 0 && sample[i] != sample[prev]) {
+            prev = p[prev - 1];
+        }
+        if (sample[i] == sample[prev]) {
+            prev++;
+        }
+        p[i] = prev;
+    }
+    return p;
+}
+
 int main() {
     char buf[1 + 1000000];
     scanf("%s", &buf);
-    std::string str(buf);
-    std::vector<int> n((int) str.length(), 0);
-    int i = 0, j = 1;
-    while (j < (int) str.length()) {
-        if (str[i] == str[j]) {
-            n[j] = i + 1;
-            j++;
-            i++;
-        } else if (i == 0) {
-            n[j] = 0;
-            j++;
-        } else {
-            i = n[i - 1];
-        }
-    }
-    for (int i = 0; i < (int) n.size(); i++) {
-        printf("%d ", n[i]);
+    std::vector<int> ans = getPrefixFunction(buf);
+    for (int i = 0; i < (int) ans.size(); i++) {
+        printf("%d ", ans[i]);
     }
     return 0;
 }
