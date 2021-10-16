d1d6daa30e7545ede07978a5c79ff28bfe743e82
Subio <subikos@mail.ru>
2021-10-16 21:28:18 +0500

    updated acmp 32

--- acmp/task-0032/code.cpp
+++ acmp/task-0032/code.cpp
@@ -2,38 +2,43 @@
 #include <string>
 #include <algorithm>
 
-// Time complexity: O(n1 * log(n1) + n2 * log(n2))
-// Space complexity: O(n1 + n2)
+// Time complexity: O(lg(n1) * log(lg(n1)) + lg(n2) * lg(log(n2)))
+// Space complexity: O(lg(n1) + lg(n2))
+
+int getMax(int n) {
+	std::string maxNum = std::to_string(n);
+	if (n < 0) {
+		std::sort(maxNum.begin(), maxNum.end());
+		int i = 1;
+		while (maxNum[i] == '0') {
+			i++;
+		}
+		std::swap(maxNum[i], maxNum[1]);
+	} else {
+		std::sort(maxNum.rbegin(), maxNum.rend());
+	}
+	return atoi(maxNum.c_str());
+}
+
+int getMin(int n) {
+	std::string minNum = std::to_string(n);
+	if (n < 0) {
+		std::sort(minNum.rbegin(), minNum.rend());
+		return -atoi(minNum.c_str());
+	} else {
+		std::sort(minNum.begin(), minNum.end());
+		int i = 0;
+		while (minNum[i] == '0') {
+			i++;
+		}
+		std::swap(minNum[i], minNum[0]);
+		return atoi(minNum.c_str());
+	}
+}
 
 int main() {
 	int n1, n2;
 	scanf("%d %d", &n1, &n2);
-	std::string maxNum1 = std::to_string(n1);
-	if (n1 < 0) {
-		std::sort(maxNum1.begin(), maxNum1.end());
-		int i = 1;
-		while (maxNum1[i] == '0') {
-			i++;
-		}
-		std::swap(maxNum1[i], maxNum1[1]);
-	} else {
-		std::sort(maxNum1.rbegin(), maxNum1.rend());
-	}
-	n1 = atoi(maxNum1.c_str());
-	std::string minNum2 = std::to_string(n2);
-	if (n2 < 0) {
-		std::sort(minNum2.rbegin(), minNum2.rend());
-		n2 = -1;
-	} else {
-		std::sort(minNum2.begin(), minNum2.end());
-		int i = 0;
-		while (minNum2[i] == '0') {
-			i++;
-		}
-		std::swap(minNum2[i], minNum2[0]);
-		n2 = 1;
-	}
-	n2 *= atoi(minNum2.c_str());
-	printf("%d", n1 - n2);
+	printf("%d", getMax(n1) - getMin(n2));
 	return 0;
 }
