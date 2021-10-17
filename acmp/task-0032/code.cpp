ea894aca21bd12e0284fbafe5ea9049aef7f3564
Subio <subikos@mail.ru>
2021-10-17 21:38:07 +0500

    improved acmp 32

--- acmp/task-0032/code.cpp
+++ acmp/task-0032/code.cpp
@@ -2,43 +2,41 @@
 #include <string>
 #include <algorithm>
 
-// Time complexity: O(lg(n1) * log(lg(n1)) + lg(n2) * lg(log(n2)))
+// Time complexity: O(lg(n1) * log(lg(n1)) + lg(n2) * log(lg(n2)))
 // Space complexity: O(lg(n1) + lg(n2))
 
-int getMax(int n) {
-	std::string maxNum = std::to_string(n);
-	if (n < 0) {
-		std::sort(maxNum.begin(), maxNum.end());
-		int i = 1;
-		while (maxNum[i] == '0') {
-			i++;
-		}
-		std::swap(maxNum[i], maxNum[1]);
-	} else {
-		std::sort(maxNum.rbegin(), maxNum.rend());
-	}
-	return atoi(maxNum.c_str());
-}
+int getMax(int n);
+
+int getMin(int n);
 
 int getMin(int n) {
-	std::string minNum = std::to_string(n);
-	if (n < 0) {
-		std::sort(minNum.rbegin(), minNum.rend());
-		return -atoi(minNum.c_str());
-	} else {
-		std::sort(minNum.begin(), minNum.end());
-		int i = 0;
-		while (minNum[i] == '0') {
-			i++;
-		}
-		std::swap(minNum[i], minNum[0]);
-		return atoi(minNum.c_str());
-	}
+    if (n < 0) {
+        return -getMax(-n);
+    } else {
+        std::string minNum = std::to_string(n);
+        std::sort(minNum.begin(), minNum.end());
+        int i = 0;
+        while (minNum[i] == '0') {
+            i++;
+        }
+        std::swap(minNum[i], minNum[0]);
+        return atoi(minNum.c_str());
+    }
+}
+
+int getMax(int n) {
+    if (n < 0) {
+        return -getMin(-n);
+    } else {
+        std::string maxNum = std::to_string(n);
+        std::sort(maxNum.rbegin(), maxNum.rend());
+        return atoi(maxNum.c_str());
+    }
 }
 
 int main() {
-	int n1, n2;
-	scanf("%d %d", &n1, &n2);
-	printf("%d", getMax(n1) - getMin(n2));
-	return 0;
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+    printf("%d", getMax(n1) - getMin(n2));
+    return 0;
 }
