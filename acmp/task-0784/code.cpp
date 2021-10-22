2746591001eae1b5d0b7cf2d28cb7580d3ba8ebb
Subio <subikos@mail.ru>
2021-10-22 20:15:14 +0500

    accepted acmp 784

---
+++ acmp/task-0784/code.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <algorithm>
+
+// Time complexity: O(log(kind1))
+// Space complexity: O(1)
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    long long kind1, kind2;
+    scanf("%lld %lld", &kind1, &kind2);
+    if (kind2 > kind1) {
+        std::swap(kind1, kind2);
+    }
+    while (kind2 <= kind1 / 2) {
+        kind1 /= 2;
+    }
+    while (kind1 != kind2) {
+        kind1 /= 2;
+        kind2 /= 2;
+    }
+    printf("%lld", kind1);
+    return 0;
+}
