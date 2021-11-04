a8d6c7c7d02e56c49a4ea4b7f586909a3e3c7a79
Subio <subikos@mail.ru>
2021-11-04 20:47:33 +0500

    updated ipc-susu 2471

--- ipc-susu/task-2471/code.cpp
+++ ipc-susu/task-2471/code.cpp
@@ -1,33 +1,25 @@
 #include <stdio.h>
 #include <algorithm>
 
-// Time complexity: O(sqrt(n) + log(n))
+// Time complexity: O(log(n))
 // Space complexity: O(1)
-
-bool isPrimeDiv(int n) {
-	for (int i = 2; i <= n / i; i++) {
-		if (n % i == 0) {
-			n /= i;
-			i = 1;
-		}
-	}
-	return n > 5;
-}
 
 int main() {
 	int n;
 	scanf("%d", &n);
-	if (n % 3 == 0 || isPrimeDiv(n)) {
+	int p1 = 0;
+	while (n % 2 == 0) {
+		n /= 2;
+		p1++;
+	}
+	int p2 = 0;
+	while (n % 5 == 0) {
+		n /= 5;
+		p2++;
+	}
+	if (n > 1) {
 		printf("NO");
 	} else {
-		int p1 = 0;
-		for (int temp = n; temp % 2 == 0; temp /= 2) {
-			p1++;
-		}
-		int p2 = 0;
-		for (int temp = n; temp % 5 == 0; temp /= 5) {
-			p2++;
-		}
 		printf("%d", std::max(p1, p2));
 	}
 	return 0;
