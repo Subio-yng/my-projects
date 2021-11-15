e9fbe9eb3d4978d7dcb93a432979e181db50ad9b
Subio <subikos@mail.ru>
2021-11-15 18:17:47 +0500

    improved acmp 16

--- acmp/task-0016/code.cpp
+++ acmp/task-0016/code.cpp
@@ -1,20 +1,30 @@
 #include <stdio.h>
+#include <vector>
 
-// Time complexity: O(n)
-// Space complexity: O(1)
+// Space complexity: O(n^2)
 
-void labber(int n, int last, int &count) {
-	for (int i = last; i < n - i; i++) {
-		labber(n - i, i + 1, count);
-		count++;
-	}
+std::vector<std::vector<int>> dp;
+
+int labber(int n, int last) {
+    if (n < 3) {
+        return 0;
+    }
+    if (dp[n][last] != -1) {
+        return dp[n][last];
+    }
+    int count = 0;
+    for (int i = last; i < n - i; i++) {
+        count += labber(n - i, i + 1);
+        count++;
+    }
+    dp[n][last] = count;
+    return count;
 }
 
 int main() {
-	int n;
-	scanf("%d", &n);
-	int count = 1;
-	labber(n, 1, count);
-	printf("%d", count);
-	return 0;
+    int n;
+    scanf("%d", &n);
+    dp.resize(n + 1, std::vector<int>(n + 1, -1));
+    printf("%d", labber(n, 1) + 1);
+    return 0;
 }
