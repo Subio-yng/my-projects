15cc0a5033529a082a2ee57e1f77a8b31911c613
Subio <subikos@mail.ru>
2021-11-15 20:32:37 +0500

    updated ipc-susu 2429

--- ipc-susu/task-2429/code.cpp
+++ ipc-susu/task-2429/code.cpp
@@ -3,11 +3,15 @@
 // Time complexity: O(1)
 // Space complexity: O(1)
 
+long long ceilDiv(long long a, long long b) {
+    return (a + b - 1) / b;
+}
+
 int main() {
     long long cookie1, price1, cookie2, price2, people;
     scanf("%lld %lld %lld %lld %lld", &cookie1, &price1, &cookie2, &price2, &people);
-    long long cost1 = (people + cookie1 - 1) / cookie1 * price1;
-    long long cost2 = (people + cookie2 - 1) / cookie2 * price2;
+    long long cost1 = ceilDiv(people, cookie1) * price1;
+    long long cost2 = ceilDiv(people, cookie2) * price2;
     if (cost1 < cost2) {
         printf("FIRST\n%lld", cost1);
     } else if (cost2 < cost1) {
