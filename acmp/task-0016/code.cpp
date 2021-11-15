9e607faae88ad9748465c0235a4e57322a513903
Subio <subikos@mail.ru>
2021-11-15 18:55:28 +0500

    updated acmp 16

--- acmp/task-0016/code.cpp
+++ acmp/task-0016/code.cpp
@@ -3,28 +3,26 @@
 
 // Space complexity: O(n^2)
 
-std::vector<std::vector<int>> dp;
-
-int labber(int n, int last) {
-    if (n < 3) {
-        return 0;
+int getCountStairs(int n,
+                   int last,
+                   std::vector<std::vector<int>> &dp) {
+    if (n == 0) {
+        return 1;
     }
-    if (dp[n][last] != -1) {
-        return dp[n][last];
+    if (dp[n][last] == -1) {
+        int count = 0;
+        for (int i = last; i <= n; i++) {
+            count += getCountStairs(n - i, i + 1, dp);
+        }
+        dp[n][last] = count;
     }
-    int count = 0;
-    for (int i = last; i < n - i; i++) {
-        count += labber(n - i, i + 1);
-        count++;
-    }
-    dp[n][last] = count;
-    return count;
+    return dp[n][last];
 }
 
 int main() {
     int n;
     scanf("%d", &n);
-    dp.resize(n + 1, std::vector<int>(n + 1, -1));
-    printf("%d", labber(n, 1) + 1);
+    std::vector<std::vector<int>> dp(1 + n, std::vector<int>(1 + n + 1, -1));
+    printf("%d", getCountStairs(n, 1, dp));
     return 0;
 }
