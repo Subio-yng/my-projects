1343c69bda9d1b83aa2d00d6c7f41fc3782c6b75
Subio <subikos@mail.ru>
2021-11-16 15:59:58 +0500

    accepted ipc-susu 2426

---
+++ ipc-susu/task-2426/code.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Time complexity: O(log(n))
+// Space complexity: O(1)
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	int k = 0;
+	while (n > 9) {
+		k++;
+		int m = 1;
+		while (n > 0) {
+			m *= n % 10;
+			n /= 10;
+		}
+		n = m;
+	}
+	printf("%d %d", k, n);
+	return 0;
+}
