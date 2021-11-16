e29c34fd2d2548697e267c030312c3ae3f01196d
Subio <subikos@mail.ru>
2021-11-16 14:58:59 +0500

    accepted ipc-susu 2427

--- ipc-susu/task-2427/code.cpp
+++ ipc-susu/task-2427/code.cpp
@@ -1,40 +1,17 @@
 #include <stdio.h>
 #include <string>
 
-// Time complexity: O(1)
+// Time complexity: O(len)
 // Space complexity: O(1)
 
-std::string plus(std::string a, std::string b) {
-    if (a.length() < b.length()) {
-        swap(a, b);
-    }
-    for (int i = (int) a.length() - 1,
-         j = (int) b.length() - 1;
-         i >= 0; i--, j--
-         ) {
-        if (j >= 0) {
-            a[i] += b[j] - '0';
-        }
-        if (a[i] > '9') {
-            a[i] -= 10;
-            if (i == 0) {
-                a = "1" + a;
-            } else {
-                a[i - 1]++;
-            }
-        }
-    }
-    return a;
-}
-
 bool isFirstClass(std::string str) {
-    const int MAX_ACTION = 10000;
+    const int MAX_ACTION = 245;
     for (int j = 0; j < MAX_ACTION; j++) {
-        std::string nextNumber;
+        int nextNumber = 0;
         for (int i = 0; i < (int) str.length(); i++) {
-            nextNumber = plus(std::to_string((str[i] - '0') * (str[i] - '0')), nextNumber);
+            nextNumber += (str[i] - '0') * (str[i] - '0');
         }
-        str = nextNumber;
+        str = std::to_string(nextNumber);
         if (str == "1") {
             break;
         }
