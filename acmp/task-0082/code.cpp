2fba4ec580f720f0650ab3d70e6a96b463fe614a
Subio <subikos@mail.ru>
2021-11-19 17:36:02 +0500

    updated acmp 82

--- acmp/task-0082/code.cpp
+++ acmp/task-0082/code.cpp
@@ -2,26 +2,27 @@
 #include <vector>
 
 // Time complexity: O(n1 + n2)
-// Space complexity: O(n1 + n2)
+// Space complexity: O(range)
 
 int main() {
     int MAXVAL = 100000;
     int n1, n2;
     scanf("%d %d", &n1, &n2);
-    std::vector<bool> list1(MAXVAL + 1, false);
+    std::vector<std::byte> list(MAXVAL + 1);
     for (int i = 0; i < n1; i++) {
         int val;
         scanf("%d", &val);
-        list1[val] = true;
+        list[val] = (std::byte) 1;
     }
-    std::vector<bool> list2(MAXVAL + 1, false);
     for (int i = 0; i < n2; i++) {
         int val;
         scanf("%d", &val);
-        list2[val] = true;
+        if (list[val] == (std::byte) 1) {
+            list[val] = (std::byte) 2;
+        }
     }
     for (int val = 0; val <= MAXVAL; val++) {
-        if (list1[val] && list2[val]) {
+        if (list[val] == (std::byte) 2) {
             printf("%d ", val);
         }
     }
