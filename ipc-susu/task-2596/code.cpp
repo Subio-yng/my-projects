31c5863818cc1fcf24fb2b445be2ecc1094b7707
Subio <subikos@mail.ru>
2021-11-25 16:50:48 +0500

    updated ipc-susu 2596

--- ipc-susu/task-2596/code.cpp
+++ ipc-susu/task-2596/code.cpp
@@ -1,38 +1,40 @@
 #include <stdio.h>
 #include <vector>
-#include <map>
+#include <unordered_map>
 
 // Time complexity: O(n)
 // Space complexity: O(n)
 
+struct Pair {
+
+	int lastId;
+
+	int count;
+
+	Pair() : lastId(-1), count(1)
+	{}
+};
+
 int main() {
 	int n, len;
 	scanf("%d %d", &n, &len);
-	std::map<int, std::vector<int>> list;
+	std::unordered_map<int, Pair> list;
+	int maxCount = 0;
+	int greaterDish = 0;
 	for (int i = 0; i < n; i++) {
 		int dish;
 		scanf("%d", &dish);
-		list[dish].push_back(i);
-	}
-	int maxCount = 0;
-	int greaterDish = 0;
-	for (auto curDish : list) {
-		int curCount = 1;
-		for (int i = 0; i < (int) curDish.second.size() - 1; i++) {
-			int j = i + 1;
-			while (j < (int) curDish.second.size() &&
-				   curDish.second[j] - curDish.second[i] <= len
-				   ) {
-				j++;
-			}
-			if (curDish.second[j] - curDish.second[i] > len) {
-				curCount++;
-			}
-			i = j - 1;
+		if (list[dish].lastId == -1) {
+			list[dish].lastId = i;
+		} else if (i - list[dish].lastId > len) {
+			list[dish].lastId = i;
+			list[dish].count++;
 		}
-		if (curCount > maxCount) {
-			greaterDish = curDish.first;
-			maxCount = curCount;
+		if (list[dish].count > maxCount ||
+			list[dish].count == maxCount && dish < greaterDish
+			) {
+			maxCount = list[dish].count;
+			greaterDish = dish;
 		}
 	}
 	printf("%d %d", greaterDish, maxCount);
