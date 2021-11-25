e4a6c962700bd85331311fe859d10752a44e87b4
Subio <subikos@mail.ru>
2021-11-25 17:30:13 +0500

    fix code-style ipc-susu 2596

--- ipc-susu/task-2596/code.cpp
+++ ipc-susu/task-2596/code.cpp
@@ -5,34 +5,34 @@
 // Time complexity: O(n)
 // Space complexity: O(n)
 
-struct Pair {
+struct DishStats {
 
 	int lastId;
 
 	int count;
 
-	Pair() : lastId(-1), count(1)
+	DishStats()
+	: lastId(-1)
+	, count(0)
 	{}
 };
 
 int main() {
 	int n, len;
 	scanf("%d %d", &n, &len);
-	std::unordered_map<int, Pair> list;
+	std::unordered_map<int, DishStats> list;
 	int maxCount = 0;
 	int greaterDish = 0;
 	for (int i = 0; i < n; i++) {
 		int dish;
 		scanf("%d", &dish);
-		if (list[dish].lastId == -1) {
-			list[dish].lastId = i;
-		} else if (i - list[dish].lastId > len) {
+		if (list[dish].lastId == -1 || i - list[dish].lastId > len) {
 			list[dish].lastId = i;
 			list[dish].count++;
 		}
 		if (list[dish].count > maxCount ||
 			list[dish].count == maxCount && dish < greaterDish
-			) {
+		) {
 			maxCount = list[dish].count;
 			greaterDish = dish;
 		}
