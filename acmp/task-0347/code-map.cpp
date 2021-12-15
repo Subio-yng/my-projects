0c98882fbe91d4410d6e0c3c42390347dd95a006
Subio <subikos@mail.ru>
2021-12-15 22:42:51 +0500

    updated acmp 544

---
+++ acmp/task-0347/code-map.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <map>
+
+// Time complexity: O(1)
+// Space complexity: O(1)
+
+int main() {
+    std::map<int> cards;
+    for (int i = 0; i < 5; i++) {
+        
+    }
+    return 0;
+}
