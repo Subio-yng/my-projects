8553add3a13a9a0096db8562217e7fc9f60891fd
Subio <subikos@mail.ru>
2021-12-25 16:09:45 +0500

    updated acmp 407

--- acmp/task-0407/code.cpp
+++ acmp/task-0407/code.cpp
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <vector>
+#include <climits>
 
 // Time complexity: O(n * k)
 // Space complexity: O(k)
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    std::vector<int> list(n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &list[i]);
+    int nCoins;
+    scanf("%d", &nCoins);
+    std::vector<int> coins(nCoins);
+    for (int i = 0; i < nCoins; i++) {
+        scanf("%d", &coins[i]);
     }
-    int k;
-    scanf("%d", &k);
-    std::vector<int> dp(k + 1, -1);
-    dp[0] = 0;
-    for (int cur = 1; cur <= k; cur++) {
-        for (int prev = 0; prev < n; prev++) {
-            if (list[prev] <= cur && dp[cur - list[prev]] > -1 &&
-                (dp[cur - list[prev]] + 1 < dp[cur] || dp[cur] == -1)
-            ) {
-                dp[cur] = dp[cur - list[prev]] + 1;
+    int reqSum;
+    scanf("%d", &reqSum);
+    std::vector<int> minCoins(reqSum + 1, INT_MAX - 1);
+    minCoins[0] = 0;
+    for (int curSum = 1; curSum <= reqSum; curSum++) {
+        for (int curCoin = 0; curCoin < nCoins; curCoin++) {
+            if (coins[curCoin] <= curSum) {
+                minCoins[curSum] = std::min(minCoins[curSum], minCoins[curSum - coins[curCoin]] + 1);
             }
         }
     }
-    printf("%d", dp[k]);
+    if (minCoins[reqSum] == INT_MAX - 1) {
+        printf("-1");
+    } else {
+        printf("%d", minCoins[reqSum]);
+    }
     return 0;
 }
