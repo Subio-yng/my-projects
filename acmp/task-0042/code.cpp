be331544de65cc8ca5eb6b0ed1205a3c8c35889d
Subio <subikos@mail.ru>
2022-01-13 22:13:04 +0500

    updated acmp 42

--- acmp/task-0042/code.cpp
+++ acmp/task-0042/code.cpp
@@ -1,19 +1,30 @@
 ﻿#include <stdio.h>
-#include <math.h>
 
-// Time complexity: O(1)
+// Time complexity: O(log(n))
 // Space complexity: O(1)
+
+long long getPow(int n, int exp) {
+    if (exp == 0) {
+        return 1;
+    }
+    long long halfExp = getPow(n, exp / 2);
+    if (exp % 2 == 0) {
+        return halfExp * halfExp;
+    } else {
+        return halfExp * halfExp * n;
+    }
+}
 
 int main() {
     int n;
     scanf("%d", &n);
     long long maxPower;
-    if (n % 3 == 0) {
-        maxPower = pow(3.0, n / 3);
+    if (n % 3 == 0 || n == 1) {
+        maxPower = getPow(3, n / 3);
     } else if (n % 3 == 1) {
-        maxPower = pow(3.0, n / 3 - 1) * 2 * 2;
+        maxPower = getPow(3, n / 3 - 1) * 2 * 2;
     } else {
-        maxPower = pow(3.0, n / 3) * 2;
+        maxPower = getPow(3, n / 3) * 2;
     }
     printf("%lld", maxPower);
     return 0;
