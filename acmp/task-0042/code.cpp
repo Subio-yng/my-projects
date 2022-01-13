1ce7c17cb0179bba508a31b76c0cd359b30048ae
Subio <subikos@mail.ru>
2022-01-13 19:22:15 +0500

    accepted acmp 42

---
+++ acmp/task-0042/code.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+// Time complexity: O(1)
+// Space complexity: O(1)
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    long long maxPower;
+    if (n % 3 == 0) {
+        maxPower = pow(3.0, n / 3);
+    } else if (n % 3 == 1) {
+        maxPower = pow(3.0, n / 3 - 1) * 2 * 2;
+    } else {
+        maxPower = pow(3.0, n / 3) * 2;
+    }
+    printf("%lld", maxPower);
+    return 0;
+}
