d289d4878c96eeb5a1c1a9457acd9868b3b1b5e5
Subio <subikos@mail.ru>
2022-01-15 18:30:21 +0500

    updated acmp 411

--- acmp/task-0411/code.cpp
+++ acmp/task-0411/code.cpp
@@ -1,31 +1,18 @@
 ﻿#include <stdio.h>
 #include <math.h>
 
+// Time complexity: O(1)
+// Space complexity: O(1)
+
 int main() {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
-    if (a == 0) {
-        if (b == 0) {
-            if (c == 0) {
-                printf("-1");
-            } else {
-                printf("0");
-            }
-        } else if (c == 0) {
-            printf("1\n0");
-        } else {
-            printf("1\n%lf", (double) -c / b);
-        }
-    } else if (b == 0) {
-        if ((double) -c / a < 0) {
-            printf("0");
-        } else if ((double) -c / a == 0) {
-            printf("1\n0");
-        } else {
-            printf("2\n%lf\n%lf", sqrt((double) -c / a), -sqrt((double) -c / a));
-        }
-    } else if (c == 0) {
-        printf("2\n0\n%lf", (double) -b / a);
+    if (a == 0 && b == 0 && c == 0) {
+        printf("-1");
+    } else if (a == 0 && b == 0 && c != 0) {
+        printf("0");
+    } else if (a == 0 && b != 0) {
+        printf("1\n%lf", (double) -c / b);
     } else {
         long long d = b * b - 1LL * 4 * a * c;
         if (d < 0) {
@@ -34,7 +21,7 @@ int main() {
             printf("1\n%lf", -b / 2.0 / a);
         } else {
             printf("2\n%lf\n%lf", (-b - sqrt(d)) / 2.0 / a,
-                   (-b + sqrt(d)) / 2.0 / a);
+                                  (-b + sqrt(d)) / 2.0 / a);
         }
     }
     return 0;
