d96be9991b156a05748e379b4f8371072151bc54
Subio <subikos@mail.ru>
2022-01-16 22:07:14 +0500

    updated acmp 1030

--- acmp/task-1030/code.cpp
+++ acmp/task-1030/code.cpp
@@ -4,8 +4,20 @@
 // Time complexity: O(log(cubes))
 // Space complexity: O(1)
 
+long long getPow(int n, int exp) {
+    if (exp == 0) {
+        return 1;
+    }
+    long long halfExp = getPow(n, exp / 2);
+    if (exp % 2 == 0) {
+        return halfExp * halfExp;
+    } else {
+        return halfExp * halfExp * n;
+    }
+}
+
 int main() {
-    const long long MAX_LEN = 2000000000000000000;
+    const long long MAX_LEN = 2 * getPow(10, 18);
     long long n1, n2, n3, n4;
     scanf("%lld %lld %lld %lld", &n1, &n2, &n3, &n4);
     long long cubes = std::min(n1, n2) + std::min(n3, n4);
