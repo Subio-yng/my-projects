e1870f5694c4123a77a299d8c3c31f45268f5278
Subio <subikos@mail.ru>
2022-01-16 21:32:25 +0500

    accepted acmp 1465

---
+++ acmp/task-1465/code.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+// Time complexity: O(1)
+// Space complexity: O(1)
+
+int main() {
+    const int MAX_NUMBER = 1000000001;
+    int left = 0;
+    int right = MAX_NUMBER;
+    char buf = ' ';
+    while (buf != '=') {
+        int mid = (left + right) / 2;
+        printf("%d\n", mid);
+        fflush(stdout);
+        scanf(" %c", &buf);
+        if (buf == '>') {
+            left = mid;
+        } else {
+            right = mid;
+        }
+    }
+    return 0;
+}
