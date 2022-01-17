0455a05a5ef839b0ddb47d1dbcf803d16d8afdad
Subio <subikos@mail.ru>
2022-01-17 21:54:34 +0500

    updated acmp 267

--- acmp/task-0267/code.cpp
+++ acmp/task-0267/code.cpp
@@ -1,28 +1,23 @@
 ﻿#include <stdio.h>
 #include <algorithm>
 
-// Time complexity: O(1)
+// Time complexity: O(log(n * max(t1, t2)))
 // Space complexity: O(1)
 
 int main() {
     int n, t1, t2;
     scanf("%d %d %d", &n, &t1, &t2);
-    int time = std::min(t1, t2);
     n--;
-    if (n == 0) {
-        printf("%d", time);
-        return 0;
-    }
-    int left = 0;
-    int right = 1000000000;
+    long long left = 0;
+    long long right = n * std::max(t1, t2);
     while (left + 1 < right) {
-        int mid = (left + right) / 2;
+        long long mid = (left + right) / 2;
         if (mid / t1 + mid / t2 < n) {
             left = mid;
         } else {
             right = mid;
         }
     }
-    printf("%d", time + right);
+    printf("%lld", std::min(t1, t2) + right);
     return 0;
 }
