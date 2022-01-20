f138eace1be4e25f3ae5845e826d0364e6aeb891
Subio <subikos@mail.ru>
2022-01-20 15:56:18 +0500

    accepted acmp 762

---
+++ acmp/task-0762/code.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+// Time complexity: O(log_3(n))
+// Space complexity: O(1)
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int count = 0;
+    while (n > 1) {
+        count++;
+        n = (n + 2) / 3;
+    }
+    printf("%d", count);
+    return 0;
+}
