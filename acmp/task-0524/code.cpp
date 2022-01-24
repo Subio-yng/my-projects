2ca6ad304d92bbff4b7ddf6d8c245c3be1f70530
Subio <subikos@mail.ru>
2022-01-24 17:01:47 +0500

    updated acmp 524

--- acmp/task-0524/code.cpp
+++ acmp/task-0524/code.cpp
@@ -10,20 +10,24 @@ bool moveBishop(int x1, int y1, int x2, int y2) {
 
 int main() {
     const int BOARD_SIZE = 8;
-    int x1, y1, x2, y2;
-    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
-    if ((x1 + y1) % 2 != (x2 + y2) % 2 || x1 == x2 && y1 == y2) {
+    int destX, destY, curX, curY;
+    scanf("%d %d %d %d", &destX, &destY, &curX, &curY);
+    if ((destX + destY) % 2 != (curX + curY) % 2 || destX == curX && destY == curY) {
         printf("0");
-    } else if (moveBishop(x1, y1, x2, y2)) {
+    } else if (moveBishop(destX, destY, curX, curY)) {
         printf("1");
     } else {
-        for (int x3 = 1; x3 <= BOARD_SIZE; x3++) {
-            for (int y3 = 1; y3 <= BOARD_SIZE; y3++) {
-                if (moveBishop(x1, y1, x3, y3) && moveBishop(x2, y2, x3, y3)) {
-                    printf("2\n%d %d", x3, y3);
-                    return 0;
-                }
-            }
+        printf("2\n");
+        int x1 = (destX - destY + curX + curY) / 2;
+        int y1 = (-destX + destY + curX + curY) / 2;
+        int x2 = (destX + destY + curX - curY) / 2;
+        int y2 = (destX + destY - curX + curY) / 2;
+        if (1 <= x1 && x1 <= BOARD_SIZE &&
+            1 <= y1 && y1 <= BOARD_SIZE
+        ) {
+            printf("%d %d", x1, y1);
+        } else {
+            printf("%d %d", x2, y2);
         }
     }
     return 0;
