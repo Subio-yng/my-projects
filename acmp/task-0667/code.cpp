c8eed4e8bea10c2ba49d0f9dd72a1bbb68e1d035
Subio <subikos@mail.ru>
2022-01-29 23:53:42 +0500

    accepted acmp 667

---
+++ acmp/task-0667/code.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+// Time complexity: O(1)
+// Space complexity: O(1)
+
+int main() {
+	int m, n, space;
+	scanf("%d %d %d", &m, &n, &space);
+	if (space < 2 || n / 2 * (space - 2) < m) {
+		printf("0");
+	} else {
+		printf("%d", (n + m + space - 1) / space);
+	}
+	return 0;
+}
