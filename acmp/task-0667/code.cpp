6f04abb1230c0794e6f7bcf3630d4c9132545c01
Subio <subikos@mail.ru>
2022-01-30 14:20:17 +0500

    updated acmp 667

--- acmp/task-0667/code.cpp
+++ acmp/task-0667/code.cpp
@@ -3,13 +3,17 @@
 // Time complexity: O(1)
 // Space complexity: O(1)
 
+int ceilDiv(int a, int b) {
+	return (a + b - 1) / b;
+}
+
 int main() {
 	int m, n, space;
 	scanf("%d %d %d", &m, &n, &space);
 	if (space < 2 || n / 2 * (space - 2) < m) {
 		printf("0");
 	} else {
-		printf("%d", (n + m + space - 1) / space);
+		printf("%d", ceilDiv(n + m, space));
 	}
 	return 0;
 }
