581d23c7e8a1500751f61f5327bba462ccd4ec4e
Subio <subikos@mail.ru>
2022-02-11 14:32:17 +0500

    accepted acmp 350

---
+++ acmp/task-0350/code.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <string>
+#include <algorithm>
+
+// Time complexity: O(len!)
+// Space complexity: O(len)
+
+int main() {
+    char buf[1 + 8];
+    scanf("%s", &buf);
+    std::string str(buf);
+    std::sort(str.begin(), str.end());
+    printf("%s", str.c_str());
+    while (std::next_permutation(str.begin(), str.end())) {
+        printf("\n%s", str.c_str());
+    }
+    return 0;
+}
