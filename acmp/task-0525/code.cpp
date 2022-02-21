751df6d348916e079af736684c79f5cbaf9988a1
Subio <subikos@mail.ru>
2022-02-21 16:18:53 +0500

    accepted acmp 525

---
+++ acmp/task-0525/code.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <vector>
+ 
+// Time complexity: O(n)
+// Space complexity: O(n)
+ 
+int main() {
+    int n;
+    scanf("%d", &n);
+    std::vector<int> ways(n + 2);
+    ways[1] = 1;
+    ways[2] = 2;
+    for (int cur = 3; cur <= n; cur++) {
+        ways[cur] = ways[cur - 2] + ways[cur / 2];
+    }
+    printf("%d", ways[n]);
+    return 0;
+}
