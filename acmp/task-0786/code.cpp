56cc71ff594bbe94a6c91c97f663aa842a3ccba4
Subio <subikos@mail.ru>
2022-02-21 16:35:45 +0500

    accepted acmp 786

---
+++ acmp/task-0786/code.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+ 
+// Time complexity: O(log(n))
+// Space complexity: O(1)
+ 
+int main() {
+    int n;
+    scanf("%d", &n);
+    int prevLose = 1;
+    while (prevLose <= n / 2) {
+        prevLose *= 2;
+    }
+    printf("%d", n - prevLose);
+    return 0;
+}
