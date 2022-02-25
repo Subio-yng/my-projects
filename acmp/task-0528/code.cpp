5a2bbdc4c9bb9de0c102119197b1e4db89452de1
Subio <subikos@mail.ru>
2022-02-25 15:09:52 +0500

    accepted acmp 528

---
+++ acmp/task-0528/code.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+ 
+// Time complexity: O(lvl)
+// Space complexity: O(1)
+ 
+int main() {
+    int n, lvl;
+    scanf("%d %d", &n, &lvl);
+    long long rooms = n;
+    int common = 3;
+    for (int curLvl = 2; curLvl <= lvl; curLvl++) {
+        rooms += n * curLvl - common;
+        common += 2;
+    }
+    printf("%lld", rooms);
+    return 0;
+}
