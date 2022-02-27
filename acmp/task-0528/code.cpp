6fdbd1eb58828936d900b8bd5aa92eea710cc734
Subio <subikos@mail.ru>
2022-02-27 12:02:05 +0500

    updated acmp 528

--- acmp/task-0528/code.cpp
+++ acmp/task-0528/code.cpp
@@ -8,7 +8,7 @@ int main() {
     scanf("%lld %lld", &n, &lvl);
     long long rooms = (n + (n - 2) * lvl) / 2 * lvl;
     if ((n + (n - 2) * lvl) % 2 == 1) {
-        rooms += 5 * lvl / 10;
+        rooms += lvl / 2;
     }
     printf("%lld", 1 + rooms);
     return 0;
