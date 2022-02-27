f953bfffc0646f214ddccb4db240bbcfc3d7800e
Subio <subikos@mail.ru>
2022-02-27 11:46:13 +0500

    updated acmp 528

--- acmp/task-0528/code.cpp
+++ acmp/task-0528/code.cpp
@@ -1,17 +1,15 @@
 ﻿#include <stdio.h>
  
-// Time complexity: O(lvl)
+// Time complexity: O(1)
 // Space complexity: O(1)
  
 int main() {
-    int n, lvl;
-    scanf("%d %d", &n, &lvl);
-    long long rooms = n;
-    int common = 3;
-    for (int curLvl = 2; curLvl <= lvl; curLvl++) {
-        rooms += n * curLvl - common;
-        common += 2;
+    long long n, lvl;
+    scanf("%lld %lld", &n, &lvl);
+    long long rooms = (n + (n - 2) * lvl) / 2 * lvl;
+    if ((n + (n - 2) * lvl) % 2 == 1) {
+        rooms += 5 * lvl / 10;
     }
-    printf("%lld", rooms);
+    printf("%lld", 1 + rooms);
     return 0;
 }
