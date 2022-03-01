511049a818e4aa3396cd309bb821a2e7824b7c94
Subio <subikos@mail.ru>
2022-03-01 20:52:31 +0500

    updated acmp 764

--- acmp/task-0764/code.cpp
+++ acmp/task-0764/code.cpp
@@ -9,14 +9,14 @@
 int main() {
     int n;
     scanf("%d", &n);
-    std::unordered_map<double, int> function;
+    std::unordered_map<double, int> angles;
     for (int i = 0; i < n; i++) {
         double x, y;
         scanf("%lf %lf", &x, &y);
-        function[std::atan2(x, y)]++;
+        angles[std::atan2(x, y)]++;
     }
     int maxTarget = 0;
-    for (auto now : function) {
+    for (auto now : angles) {
         maxTarget = std::max(maxTarget, now.second);
     }
     printf("%d", maxTarget);
