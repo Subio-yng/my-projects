ba54379aaf202c2af41d431a759ebd3cf3f3d133
Subio <subikos@mail.ru>
2022-03-02 21:39:33 +0500

    updated acmp 181

--- acmp/task-0181/code.cpp
+++ acmp/task-0181/code.cpp
@@ -1,16 +1,15 @@
 ﻿#include <stdio.h>
 #include <unordered_map>
-#include <vector>
 #include <string>
 
 // Time complexity: O(n)
-// Space complexity: O(n)
+// Space complexity: O(1)
  
 struct Rule {
 
     std::string str;
 
-    std::vector<int> move;
+    int move;
 
     static Rule read() {
         std::string s;
@@ -20,16 +19,9 @@ struct Rule {
             s += buf;
             scanf("%c", &buf);
         }
-        return {s, {1}};
+        return {s, 1};
     }
 };
-
-void updateLastMove(std::unordered_map<char, Rule> &list, char a, int n) {
-    list[a].move.push_back(1);
-    for (int i = 0; i < (int) list[a].str.length(); i++) {
-        list[a].move[n] += list[list[a].str[i]].move[n - 1];
-    }
-}
 
 int main() {
     const std::string DIRECT = "NSWEUD";
@@ -41,10 +33,14 @@ int main() {
     char direction;
     scanf("%c %d", &direction, &n);
     for (int cur = 1; cur < n; cur++) {
+        auto copy = listRules;
         for (int i = 0; i < (int) DIRECT.length(); i++) {
-            updateLastMove(listRules, DIRECT[i], cur);
+            listRules[DIRECT[i]].move = 1;
+            for (int j = 0; j < (int) listRules[DIRECT[i]].str.length(); j++) {
+                listRules[DIRECT[i]].move += copy[listRules[DIRECT[i]].str[j]].move;
+            }
         }
     }
-    printf("%d", listRules[direction].move[n - 1]);
+    printf("%d", listRules[direction].move);
     return 0;
 }
