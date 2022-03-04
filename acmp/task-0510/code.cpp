53b5ac040699af197851e5721f0e89bca71ca129
Subio <subikos@mail.ru>
2022-03-04 14:05:09 +0500

    updated acmp 510

--- acmp/task-0510/code.cpp
+++ acmp/task-0510/code.cpp
@@ -7,13 +7,18 @@
 int main() {
     int n;
     scanf("%d", &n);
+    if (n % 2 == 1) {
+        printf("0");
+        return 0;
+    }
+    n /= 2;
     std::vector<int> ways(n + 1, 0);
     ways[0] = 1;
-    for (int cur = 2; cur <= n; cur += 2) {
-        if (cur == 2) {
-            ways[cur] = ways[cur - 2] + 2 * ways[cur - 2];
+    for (int cur = 1; cur <= n; cur++) {
+        if (cur == 1) {
+            ways[cur] = ways[cur - 1] + 2 * ways[cur - 1];
         } else {
-            ways[cur] = 4 * ways[cur - 2] - ways[cur - 4];
+            ways[cur] = 4 * ways[cur - 1] - ways[cur - 2];
         }
     }
     printf("%d", ways[n]);
