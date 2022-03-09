1365711743c9acd223968fa62b6699b938ffe4ab
Subio <subikos@mail.ru>
2022-03-09 19:06:33 +0500

    updated acmp 1047

--- acmp/task-1047/code.cpp
+++ acmp/task-1047/code.cpp
@@ -1,6 +1,7 @@
 ﻿#include <stdio.h>
 #include <vector>
 #include <algorithm>
+#include <climits>
 
 // Time complexity: O(n! * n)
 // Space complexity: O(n^2)
@@ -10,21 +11,20 @@ int main() {
     scanf("%d", &n);
     std::vector<std::vector<int>> table(n, std::vector<int>(n));
     std::vector<int> worker(n);
-    int minSalary = 0;
     for (int i = 0; i < n; i++) {
         worker[i] = i;
         for (int j = 0; j < n; j++) {
             scanf("%d", &table[i][j]);
         }
-        minSalary += table[i][i];
     }
-    while (std::next_permutation(worker.begin(), worker.end())) {
+    int minSalary = INT_MAX;
+    do {
         int curSalary = 0;
         for (int i = 0; i < n; i++) {
             curSalary += table[worker[i]][i];
         }
         minSalary = std::min(minSalary, curSalary);
-    }
+    } while (std::next_permutation(worker.begin(), worker.end()));
     printf("%d", minSalary);
     return 0;
 }
