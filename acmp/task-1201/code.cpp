04cccc8b54a9b90fb135fda08c81f9a3b1bfaa80
Subio <subikos@mail.ru>
2022-03-09 18:55:54 +0500

    updated acmp 1201

--- acmp/task-1201/code.cpp
+++ acmp/task-1201/code.cpp
@@ -5,8 +5,25 @@
 // Space complexity: O(n^2)
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    
+    int nV;
+    scanf("%d", &nV);
+    std::vector<std::vector<int>> list(nV + 1);
+    int nE = 0;
+    for (int i = 1; i <= nV; i++) {
+        for (int j = 1; j <= nV; j++) {
+            char val;
+            scanf(" %c", &val);
+            if (val == '1') {
+                list[i].push_back(j);
+                nE++;
+            }
+        }
+    }
+    printf("%d %d", nV, nE);
+    for (int i = 1; i <= nV; i++) {
+        for (int j = 0; j < (int) list[i].size(); j++) {
+            printf("\n%d %d", i, list[i][j]);
+        }
+    }
     return 0;
 }
