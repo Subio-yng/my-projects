0ca870ff027583dd5fbb0db5bb4becd8ba7138c4
Subio <subikos@mail.ru>
2022-03-09 17:28:53 +0500

    accepted acmp 1201

---
+++ acmp/task-1201/code.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <vector>
+
+// Time complexity: O(n^2)
+// Space complexity: O(n^2)
+
+struct Graph {
+
+    int in;
+
+    int out;
+};
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    std::vector<Graph> list;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            char val;
+            scanf(" %c", &val);
+            if (val == '1') {
+                list.push_back({i, j});
+            }
+        }
+    }
+    printf("%d %d", n, (int) list.size());
+    for (int i = 0; i < (int) list.size(); i++) {
+        printf("\n%d %d", list[i].in, list[i].out);
+    }
+    return 0;
+}
