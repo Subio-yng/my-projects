fa7d0b3971a683af40e50a3b08ba9888ea73e35a
Subio <subikos@mail.ru>
2022-03-09 18:54:40 +0500

    fix code-style acmp 1201

--- acmp/task-1201/code.cpp
+++ acmp/task-1201/code.cpp
@@ -7,23 +7,6 @@
 int main() {
     int n;
     scanf("%d", &n);
-    std::vector<std::vector<int>> list(n + 1);
-    int edge = 0;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            char val;
-            scanf(" %c", &val);
-            if (val == '1') {
-                list[i].push_back(j);
-                edge++;
-            }
-        }
-    }
-    printf("%d %d", n, edge);
-    for (int i = 1; i <= n; i++) {
-        for (int j = 0; j < (int) list[i].size(); j++) {
-            printf("\n%d %d", i, list[i][j]);
-        }
-    }
+    
     return 0;
 }
