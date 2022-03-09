c803b9b5f2a1a516ce1ba4461ca0e3bce7eb52db
Subio <subikos@mail.ru>
2022-03-09 15:46:57 +0500

    accepted acmp 1047

---
+++ acmp/task-1047/code.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+// Time complexity: O(n! * n)
+// Space complexity: O(n^2)
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    std::vector<std::vector<int>> table(n, std::vector<int>(n));
+    std::vector<int> worker(n);
+    int minSalary = 0;
+    for (int i = 0; i < n; i++) {
+        worker[i] = i;
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &table[i][j]);
+        }
+        minSalary += table[i][i];
+    }
+    while (std::next_permutation(worker.begin(), worker.end())) {
+        int curSalary = 0;
+        for (int i = 0; i < n; i++) {
+            curSalary += table[worker[i]][i];
+        }
+        minSalary = std::min(minSalary, curSalary);
+    }
+    printf("%d", minSalary);
+    return 0;
+}
