5a9ed1235047a5bbb48f3d44ebad4a415101f876
Subio <subikos@mail.ru>
2022-03-15 20:57:42 +0500

    accepted acmp 1205

---
+++ acmp/task-1205/code.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+ 
+// Time complexity: O(1)
+// Space complexity: O(1)
+ 
+int main() {
+    int nV;
+    scanf("%d", &nV);
+    int prevPowV = -1;
+    int curPowV = 0;
+    for (int i = 0; i < nV; i++) {
+        for (int j = 0; j < nV; j++) {
+            char val;
+            scanf(" %c", &val);
+            if (val == '1') {
+                curPowV++;
+            }
+        }
+        if (prevPowV != -1 && prevPowV != curPowV) {
+            printf("NO");
+            return 0;
+        }
+        prevPowV = curPowV;
+        curPowV = 0;
+    }
+    printf("YES");
+    return 0;
+}
