77149d8991cb2bb3660fd11d71459cd63c350815
Subio <subikos@mail.ru>
2022-04-16 22:20:15 +0500

    accepted acmp 1361

--- acmp/task-1361/code.cpp
+++ acmp/task-1361/code.cpp
@@ -1,48 +1,48 @@
 #include <stdio.h>
 #include <vector>
 #include <queue>
-#include <map>
-
+ 
 // Time complexity: O(1)
 // Space complexity: O(1)
+ 
+const int SIZE = 1000;
+const int COUNT_STATE = 4;
+const int UNDEF = -1;
+const int START_POINT = 0;
 
-const int LENGTH = 1000;
-
-void bfs(int start, int end) {
-	std::map<int, std::vector<int>> way;
-	std::queue<int> inProcess;
-	inProcess.push(start);
-	while (!inProcess.empty()) {
-		int cur = inProcess.front();
-		inProcess.pop();
-		way[cur].push_back(cur);
-		if (cur / LENGTH < 9 && way[cur + LENGTH].empty()) {
-			way[cur + LENGTH] = way[cur];
-			inProcess.push(cur + LENGTH);
-		}
-		if (cur % 10 > 1 && way[cur - 1].empty()) {
-			way[cur - 1] = way[cur];
-			inProcess.push(cur - 1);
-		}
-		int rightShift = cur % 10 * LENGTH + cur / 10;
-		if (way[rightShift].empty()) {
-			way[rightShift] = way[cur];
-			inProcess.push(rightShift);
-		}
-		int leftShift = cur % LENGTH * 10 + cur / LENGTH;
-		if (way[leftShift].empty()) {
-			way[leftShift] = way[cur];
-			inProcess.push(leftShift);
-		}
-	}
-	for (auto next : way[end]) {
-		printf("%d\n", next);
-	}
+void printWay(std::vector<int> from, int last) {
+    if (from[last] != START_POINT) {
+        printWay(from, from[last]);
+    }
+    printf("%d\n", last);
 }
 
+void bfs(int start, int end) {
+    std::vector<int> next(COUNT_STATE);
+    std::vector<int> from(SIZE * 10, UNDEF);
+    std::queue<int> inProcess;
+    inProcess.push(start);
+    from[start] = START_POINT;
+    while (!inProcess.empty()) {
+        int cur = inProcess.front();
+        inProcess.pop();
+        next[0] = cur + SIZE;
+        next[1] = cur - 1;
+        next[2] = cur % 10 * SIZE + cur / 10;
+        next[3] = cur % SIZE * 10 + cur / SIZE;
+        for (int i = 0; i < COUNT_STATE; i++) {
+            if (next[i] % 10 != 0 && next[i] < SIZE * 10 && from[next[i]] == UNDEF) {
+                from[next[i]] = cur;
+                inProcess.push(next[i]);
+            }
+        }
+    }
+    printWay(from, end);
+}
+ 
 int main() {
-	int start, end;
-	scanf("%d %d", &start, &end);
-	bfs(start, end);
-	return 0;
+    int start, end;
+    scanf("%d %d", &start, &end);
+    bfs(start, end);
+    return 0;
 }
