3d8ac44c11ed2526d3898618de484fec69493669
Subio <subikos@mail.ru>
2022-04-17 19:30:54 +0500

    updated acmp 899

--- acmp/task-0899/code.cpp
+++ acmp/task-0899/code.cpp
@@ -2,40 +2,43 @@
 #include <string>
 #include <stack>
 
+// Time complexity: O(len)
+// Space complexity: O(len)
+
 char getOppositeBracket(char a) {
-	if (a == ')') {
-		return '(';
-	} else if (a == ']') {
-		return '[';
-	} else if (a == '}') {
-		return '{';
-	} else {
-		throw 1;
-	}
+    if (a == ')') {
+        return '(';
+    } else if (a == ']') {
+        return '[';
+    } else if (a == '}') {
+        return '{';
+    } else {
+        throw 1;
+    }
 }
-
+ 
 bool checkLine(const std::string &str) {
-	std::stack<char> last;
-	for (char cur : str) {
-		if (last.empty() || cur == '(' || cur == '[' || cur == '{') {
-			last.push(cur);
-		} else if (last.top() == getOppositeBracket(cur)) {
-			last.pop();
-		} else {
-			return false;
-		}
-	}
-	return last.empty();
+    std::stack<char> last;
+    for (char cur : str) {
+        if (last.empty() || cur == '(' || cur == '[' || cur == '{') {
+            last.push(cur);
+        } else if (last.top() == getOppositeBracket(cur)) {
+            last.pop();
+        } else {
+            return false;
+        }
+    }
+    return last.empty();
 }
-
+ 
 int main() {
-	char buf[1 + 255] = {};
-	while (scanf("%255s", &buf) != EOF) {
-		if (checkLine(buf)) {
-			printf("0");
-		} else {
-			printf("1");
-		}
-	}
-	return 0;
+    char buf[1 + 255] = {};
+    while (scanf("%255s", &buf) != EOF) {
+        if (checkLine(buf)) {
+            printf("0");
+        } else {
+            printf("1");
+        }
+    }
+    return 0;
 }
