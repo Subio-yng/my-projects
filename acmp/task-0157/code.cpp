c92767ce0d9e1838a8b98c92cdd0ceb0661733c9
Subio <subikos@mail.ru>
2022-05-17 16:58:38 +0500

    accepted acmp 157

---
+++ acmp/task-0157/code.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <vector>
+#include <string>
+
+// Time complexity: O(len)
+// Space complexity: O(len)
+
+long long getFactorial(int n) {
+	long long res = n;
+	for (int i = 2; i < n; i++) {
+		res *= i;
+	}
+	return res;
+}
+
+int main() {
+	const int ALPHABET_SIZE = 26;
+	char buf[1 + 15];
+	scanf("%s", &buf);
+	std::string str(buf);
+	std::vector<int> words(ALPHABET_SIZE, 0);
+	for (char next : str) {
+		words[next - 'a']++;
+	}
+	long long ans = getFactorial((int) str.length());
+	for (int i = 0; i < ALPHABET_SIZE; i++) {
+		if (words[i] > 0) {
+			ans /= getFactorial(words[i]);
+		}
+	}
+	printf("%lld", ans);
+	return 0;
+}
