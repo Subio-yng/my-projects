0374c8885cfee6860d6a3479864c75318adf1d9d
Subio <subikos@mail.ru>
2022-05-25 17:06:20 +0500

    accepted acmp 1378

---
+++ acmp/task-1378/code.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+// Time complexity: O(nV * nE)
+// Space complexity: O(nE)
+
+struct Edge {
+
+	int fromV;
+
+	int fromT;
+
+	int toV;
+
+	int toT;
+
+	static Edge read() {
+		int fV, fT, tV, tT;
+		scanf("%d %d %d %d", &fV, &fT, &tV, &tT);
+		return {fV - 1, fT, tV - 1, tT};
+	}
+};
+
+int main() {
+	int nV;
+	scanf("%d", &nV);
+	int startV;
+	int endV;
+	scanf("%d %d", &startV, &endV);
+	startV--;
+	endV--;
+	int nE;
+	scanf("%d", &nE);
+	std::vector<Edge> edges(nE);
+	for (int i = 0; i < nE; i++) {
+		edges[i] = Edge::read();
+	}
+	const int INF = 1'000'000'000 + 1;
+	std::vector<int> dist(nV, INF);
+	dist[startV] = 0;
+	bool hasChanged = true;
+	while (hasChanged) {
+		hasChanged = false;
+		for (Edge nextE : edges) {
+			if (dist[nextE.fromV] <= nextE.fromT && dist[nextE.toV] > nextE.toT) {
+				dist[nextE.toV] = nextE.toT;
+				hasChanged = true;
+			}
+		}
+	}
+	printf("%d", dist[endV]);
+	return 0;
+}
