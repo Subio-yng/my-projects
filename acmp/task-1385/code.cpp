8006f2d6086c4ab7eb9242da4e0f87344018f4d4
Subio <subikos@mail.ru>
2022-06-17 19:09:13 +0500

    updated acmp 1385

--- acmp/task-1385/code.cpp
+++ acmp/task-1385/code.cpp
@@ -1,64 +1,57 @@
 #include <stdio.h>
 #include <vector>
-
-// Time complexity: O(a(nV))
+ 
+// Time complexity: O(nV)
 // Space complexity: O(nV)
-
+ 
 struct DSU {
-
-	std::vector<int> parent;
-
-	std::vector<int> rank;
-
-	DSU(int nV) {
-		parent.resize(nV);
-		rank.resize(nV);
-		for (int i = 0; i < nV; i++) {
-			parent[i] = i;
-			rank[i] = 0;
-		}
-	}
-
-	int get(int n) {
-		int leader = n;
-		while (parent[leader] != leader) {
-			leader = parent[leader];
-		}
-		while (parent[n] != n) {
-			n = parent[n];
-			parent[n] = leader;
-		}
-		return leader;
-	}
-
-	bool union_sets(int a, int b) {
-		a = get(a);
-		b = get(b);
-		if (a != b) {
-			if (rank[a] < rank[b]) {
-				std::swap(a, b);
-			}
-			parent[b] = a;
-			if (rank[a] == rank[b]) {
-				rank[a]++;
-			}
-		}
-		return a != b;
-	}
+ 
+    std::vector<int> parent;
+ 
+    std::vector<int> rank;
+ 
+    DSU(int nV) {
+        parent.resize(nV);
+        rank.resize(nV);
+        for (int i = 0; i < nV; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+ 
+    int getRoot(int n) {
+        if (parent[n] == n) {
+            return n;
+        }
+        return parent[n] = getRoot(parent[n]);
+    }
+ 
+    bool unionSets(int a, int b) {
+        a = getRoot(a);
+        b = getRoot(b);
+        if (a != b) {
+            if (rank[a] < rank[b]) {
+                std::swap(a, b);
+            }
+            parent[b] = a;
+            if (rank[a] == rank[b]) {
+                rank[a]++;
+            }
+        }
+        return a != b;
+    }
 };
-
+ 
 int main() {
-	int nV, nE;
-	scanf("%d %d", &nV, &nE);
-	DSU dsu(nV);
-	for (int i = 0; i < nE; i++) {
-		int from, to;
-		scanf("%d %d", &from, &to);
-		from--;
-		to--;
-		if (dsu.union_sets(from, to)) {
-			printf("%d %d\n", from + 1, to + 1);
-		}
-	}
-	return 0;
+    int nV, nE;
+    scanf("%d %d", &nV, &nE);
+    DSU dsu(nV);
+    for (int i = 0; i < nE; i++) {
+        int from, to;
+        scanf("%d %d", &from, &to);
+        if (dsu.unionSets(from - 1, to - 1)) {
+            printf("%d %d\n", from, to);
+        }
+    }
+    return 0;
 }
