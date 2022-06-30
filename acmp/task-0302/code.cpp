c3043b9a9e88e73941a605b5c696d15ca36fa2c0
Subio <subikos@mail.ru>
2022-06-30 11:58:53 +0500

    accepted acmp 302

---
+++ acmp/task-0302/code.cpp
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+
+// Time complexity: O(nV^2 * log(range))
+// Space complexity: O(nV)
+
+struct DSU {
+
+	std::vector<int> parent;
+
+	std::vector<int> rank;
+
+	DSU(int nV) {
+		parent.resize(nV);
+		rank.resize(nV);
+		for (int i = 0; i < nV; i++) {
+			parent[i] = i;
+			rank[i] = 0;
+		}
+	}
+
+	int getRoot(int n) {
+		if (parent[n] == n) {
+			return n;
+		}
+		return parent[n] = getRoot(parent[n]);
+	}
+
+	bool unionSets(int a, int b) {
+		a = getRoot(a);
+		b = getRoot(b);
+		if (a != b) {
+			if (rank[b] > rank[a]) {
+				std::swap(a, b);
+			}
+			parent[b] = a;
+			if (rank[a] == rank[b]) {
+				rank[a]++;
+			}
+		}
+		return a != b;
+	}
+};
+
+struct Coord {
+	
+	double x;
+
+	double y;
+
+	static Coord read() {
+		double x, y;
+		scanf("%lf %lf", &x, &y);
+		return {x, y};
+	}
+
+	double getDistTo(const Coord &a) const {
+		return sqrt(pow(x - a.x, 2) + pow(y - a.y, 2));
+	}
+};
+
+int main() {
+	int nV;
+	scanf("%d", &nV);
+	std::vector<Coord> field(nV);
+	for (int i = 0; i < nV; i++) {
+		field[i] = Coord::read();
+	}
+	double rightDist = 0;
+	for (int i = 0; i < nV - 1; i++) {
+		for (int j = i + 1; j < nV; j++) {
+			rightDist = std::max(rightDist, field[i].getDistTo(field[j]));
+		}
+	}
+	double leftDist = 0;
+	while (leftDist + 0.01 < rightDist) {
+		double midDist = (leftDist + rightDist) / 2.0;
+		DSU dsu(nV);
+		int count = 0;
+		for (int i = 0; i < nV - 1; i++) {
+			for (int j = i + 1; j < nV; j++) {
+				if (field[i].getDistTo(field[j]) <= midDist && dsu.unionSets(i, j)) {
+					count++;
+				}
+			}
+		}
+		if (count == nV - 1) {
+			rightDist = midDist;
+		} else {
+			leftDist = midDist;
+		}
+	}
+	printf("%.2f", rightDist);
+	return 0;
+}
