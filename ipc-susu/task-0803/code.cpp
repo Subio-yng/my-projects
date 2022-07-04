01d3e06e5e5381e23e5f7ac002f0e36d6563b880
Subio <subikos@mail.ru>
2022-07-04 18:08:06 +0500

    accepted ipc-susu 803

---
+++ ipc-susu/task-0803/code.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+// Time complexity: O(nE * log(nE))
+// Space complexity: O(nE)
+
+struct DSU {
+
+    std::vector<int> parent;
+
+    std::vector<int> rank;
+
+    DSU(int nV) {
+        parent.resize(nV);
+        rank.resize(nV);
+        for (int i = 0; i < nV; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    int getRoot(int n) {
+        if (parent[n] == n) {
+            return n;
+        }
+        return parent[n] = getRoot(parent[n]);
+    }
+
+    bool unionSets(int a, int b) {
+        a = getRoot(a);
+        b = getRoot(b);
+        if (a != b) {
+            if (rank[a] < rank[b]) {
+                std::swap(a, b);
+            }
+            parent[b] = a;
+            if (rank[a] == rank[b]) {
+                rank[a]++;
+            }
+        }
+        return a != b;
+    }
+};
+
+struct Edge {
+
+    int from;
+
+    int to;
+
+    int weight;
+
+    static Edge read() {
+        int f, t, w;
+        scanf("%d %d %d", &f, &t, &w);
+        return {f - 1, t - 1, w};
+    }
+};
+
+int main() {
+    int nV, nE;
+    scanf("%d %d", &nV, &nE);
+    std::vector<Edge> edges(nE);
+    for (int i = 0; i < nE; i++) {
+        edges[i] = Edge::read();
+    }
+    std::sort(edges.begin(), edges.end(), [](const Edge &left, const Edge &right) {
+        if (left.weight == right.weight) {
+            if (left.from == right.from) {
+                return left.to < right.to;
+            }
+            return left.from < right.from;
+        }
+        return left.weight < right.weight;
+    });
+    DSU dsu(nV);
+    std::vector<Edge> used;
+    int maxWeight = 0;
+    for (Edge nextE : edges) {
+        if (dsu.unionSets(nextE.from, nextE.to)) {
+            maxWeight = nextE.weight;
+            used.push_back(nextE);
+        }
+    }
+    printf("%d\n", maxWeight);
+    printf("%d", (int) used.size());
+    for (Edge nextE : used) {
+        printf("\n%d %d", nextE.from + 1, nextE.to + 1);
+    }
+	return 0;
+}
