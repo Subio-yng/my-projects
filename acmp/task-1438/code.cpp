04b60c8ac7e6539b51209a4d63f3a40c85a36572
Subio <subikos@mail.ru>
2022-07-05 13:49:16 +0500

    accepted acmp 1438

---
+++ acmp/task-1438/code.cpp
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+// Time complexity: O(nE * log(nE))
+// Space complexity: O(nE)
+
+struct DSU {
+
+    std::vector<int> parent;
+
+    std::vector<int> rank;
+
+    DSU(int nV) {
+        parent.resize(nV);
+        rank.resize(nV);
+        for (int i = 0; i < nV; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    int getRoot(int n) {
+        if (parent[n] == n) {
+            return n;
+        }
+        return parent[n] = getRoot(parent[n]);
+    }
+
+    int unionSets(int a, int b) {
+        a = getRoot(a);
+        b = getRoot(b);
+        if (a != b) {
+            if (rank[b] > rank[a]) {
+                std::swap(a, b);
+            }
+            parent[b] = a;
+            rank[a] += rank[b];
+        }
+        return rank[a];
+    }
+};
+
+struct Edge {
+
+    int from;
+
+    int to;
+
+    int weight;
+
+    static Edge read() {
+        int f, t, w;
+        scanf("%d %d %d", &f, &t, &w);
+        return {f - 1, t - 1, w};
+    }
+};
+
+int main() {
+    int nV, nE, needV;
+    scanf("%d %d %d", &nV, &nE, &needV);
+    std::vector<Edge> edges(nE);
+    for (int i = 0; i < nE; i++) {
+        edges[i] = Edge::read();
+    }
+    std::sort(edges.begin(), edges.end(), [](const Edge &left, const Edge &right) {
+        if (left.weight == right.weight) {
+            if (left.from == right.from) {
+                return left.to > right.to;
+            }
+            return left.from > right.from;
+        }
+        return left.weight > right.weight;
+    });
+    DSU dsu(nV);
+    for (Edge nextE : edges) {
+        if (dsu.unionSets(nextE.from, nextE.to) > needV) {
+            printf("%d", nextE.weight);
+            break;
+        }
+    }
+    return 0;
+}
