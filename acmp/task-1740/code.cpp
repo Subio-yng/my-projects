a94769244dd64895d2e6c72f05cf5bc9af32e77e
Subio <subikos@mail.ru>
2022-12-09 14:30:45 +0500

    updated acmp 1740

--- acmp/task-1740/code.cpp
+++ acmp/task-1740/code.cpp
@@ -6,15 +6,23 @@
 int main() {
     long long n;
     scanf("%lld", &n);
+    const int UNDEF = -1;
+    long long x = UNDEF, y = UNDEF;
     if (n > 1 && n % 2 == 1) {
+        x = (n + 1) / 2;
+        y = (n - 1) / 2;
+    }
+    if (n > 4 && n % 4 == 0) {
+        x = n / 4 + 1;
+        y = n / 4 - 1;
+    }
+    if (n == 0) {
+        x = 1;
+        y = 1;
+    }
+    if (x != UNDEF) {
         printf("Yes\n");
-        printf("%lld %lld", (n + 1) / 2, (n - 1) / 2);
-    } else if (n > 4 && n % 4 == 0) {
-        printf("Yes\n");
-        printf("%lld %lld", n / 4 + 1, n / 4 - 1);
-    } else if (n == 0) {
-        printf("Yes\n");
-        printf("1 1");
+        printf("%lld %lld", x, y);
     } else {
         printf("No");
     }
