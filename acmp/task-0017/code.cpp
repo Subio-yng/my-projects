b35ff2007ffecefc01c99b5992f96db04644ded7
Subio <subikos@mail.ru>
2023-09-06 18:22:40 +0500

    updated acmp 17

--- acmp/task-0017/code.cpp
+++ acmp/task-0017/code.cpp
@@ -1,27 +1,37 @@
 #include <bits/stdc++.h>
 
-// Time complexity: O(n * log(n))
+// Time complexity: O(n)
 // Space complexity: O(n)
+
+std::vector<int> getZFunction(const std::vector<int> &array, int n) {
+    std::vector<int> z(n, 0);
+    int left = 0;
+    int right = 0;
+    for (int i = 1; i < n; i++) {
+        if (i <= right) {
+            z[i] = std::min(right - i + 1, z[i - left]);
+        }
+        while (z[i] + i < n && array[z[i]] == array[z[i] + i]) {
+            z[i]++;
+        }
+        if (z[i] + i - 1 > right) {
+            left = i;
+            right = z[i] + i - 1;
+        }
+    }
+    return z;
+}
 
 int main() {
     int n;
     scanf("%d", &n);
-    std::vector<int> arr(n);
+    std::vector<int> array(n);
     for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+        scanf("%d", &array[i]);
     }
+    std::vector<int> z = getZFunction(array, n);
     for (int i = 1; i < n; i++) {
-        if ((n - 1) % i != 0) {
-            continue;
-        }
-        bool findAns = true;
-        for (int j = i; j < n; j++) {
-            if (arr[j] != arr[j - i]) {
-                findAns = false;
-                break;
-            }
-        }
-        if (findAns) {
+        if ((n - 1) % i == 0 && z[i] == n - i) {
             printf("%d", i);
             return 0;
         }
