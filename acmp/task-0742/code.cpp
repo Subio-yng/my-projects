f237a7bfc8a489a8c54cac5ca49d4a77f49a37fb
Subio <subikos@mail.ru>
2023-11-27 15:58:05 +0500

    updated acmp 742

--- acmp/task-0742/code.cpp
+++ acmp/task-0742/code.cpp
@@ -6,65 +6,135 @@ private:
 
     static const int MOD = 1'000'000'000;
 
-    std::vector<int> number;
+    static const int CELL_SIZE = 9;
 
-    const void addResidue(int last, int residue) {
-        while (0 < residue && last < (int) number.size()) {
-            number[last] += residue;
+    static const BigInteger ZERO() {
+        return valueOf(0);
+    }
+
+    static const BigInteger ONE() {
+        return valueOf(1);
+    }
+
+    static const BigInteger TEN() {
+        return valueOf(10);
+    }
+
+    short sign;
+
+    std::vector<int> value;
+
+    void addResidue(int last, int residue) {
+        while (0 < residue && last < (int) value.size()) {
+            value[last] += residue;
             residue = 0;
-            if (number[last] >= MOD) {
-                number[last] -= MOD;
+            if (value[last] >= MOD) {
+                value[last] -= MOD;
                 residue = 1;
             }
             last++;
         }
         if (0 < residue) {
-            number.push_back(residue);
+            value.push_back(residue);
         }
     }
 
 public:
 
     BigInteger()
+            : sign(1)
     {}
 
-    BigInteger(int n) {
-        number.push_back(n);
-        if (n >= MOD) {
-            number.back() %= MOD;
-            number.push_back(n / MOD);
+    BigInteger(const short &setSign, const std::vector<int> &setValue)
+            : sign(setSign)
+            , value(setValue)
+    {}
+
+    static BigInteger valueOf(long long num) {
+        short sign = 1;
+        if (num < 0) {
+            sign = -1;
+            num *= -1;
         }
+        std::vector<int> value(1, num % MOD);
+        if (num >= MOD) {
+            value.push_back(num / MOD);
+        }
+        return {sign, value};
     }
 
-    BigInteger(const std::string str) {
-        number.push_back(0);
-        int mult = 1;
-        for (int i = (int) str.length() - 1; i >= 0; i--) {
-            if (mult == MOD) {
-                mult = 1;
-                number.push_back(0);
+    static BigInteger valueOf(const std::string &str) {
+        short sign = 1;
+        int last = 0;
+        if (str[last] == '-') {
+            sign = -1;
+            last = 1;
+        }
+        std::vector<int> value;
+        int right = (int) str.length();
+        while (right >= last) {
+            int left = std::max(0, right - CELL_SIZE);
+            value.push_back(std::abs(atoi(str.substr(left, right - left).c_str())));
+            right -= CELL_SIZE;
+        }
+        if ((int) value.size() > 1 && value.back() == 0) {
+            value.pop_back();
+        }
+        return {sign, value};
+    }
+
+    void operator =(const BigInteger &a) {
+        sign = a.sign;
+        value = a.value;
+    }
+
+    bool operator ==(const BigInteger &a) const {
+        return sign == a.sign && value == a.value;
+    }
+
+    bool operator <(const BigInteger &a) const {
+        if ((int) value.size() != (int) a.value.size() || sign != a.sign) {
+            return sign * value.size() < a.sign * a.value.size();
+        }
+        for (int i = (int) value.size() - 1; i >= 0; i--) {
+            if (value[i] != a.value[i]) {
+                return sign * value[i] < a.sign * a.value[i];
             }
-            number.back() += (str[i] - '0') * mult;
-            mult *= 10;
         }
+        return false;
     }
 
-    const void operator =(const BigInteger &a) {
-        number = a.number;
+    bool operator <=(const BigInteger &a) const {
+        return *this == a || *this < a;
     }
 
-    const BigInteger operator +(BigInteger a) const {
+    bool operator >(const BigInteger &a) const {
+        return a < *this;
+    }
+
+    bool operator >=(const BigInteger &a) const {
+        return a <= *this;
+    }
+
+    static BigInteger abs(const BigInteger &a) {
+        return {a.sign * a.sign, a.value};
+    }
+
+    BigInteger operator +(const BigInteger &a) const {
+        if (sign != a.sign) {
+            return *this - -a;
+        }
+        if (value.size() < a.value.size()) {
+            return a + *this;
+        }
         BigInteger res = *this;
-        if (a.number.size() > res.number.size()) {
-            std::swap(a, res);
-        }
         int residue = 0;
         int last = 0;
-        while (last < (int) a.number.size()) {
-            res.number[last] += a.number[last] + residue;
+        while (last < (int) a.value.size()) {
+            res.value[last] += a.value[last] + residue;
             residue = 0;
-            if (res.number[last] >= MOD) {
-                res.number[last] -= MOD;
+            if (res.value[last] >= MOD) {
+                res.value[last] -= MOD;
                 residue = 1;
             }
             last++;
@@ -73,67 +143,257 @@ public:
         return res;
     }
 
-    const void operator +=(BigInteger a) {
+    BigInteger operator +(long long num) const {
+        return *this + valueOf(num);
+    }
+
+    void operator +=(const BigInteger &a) {
         *this = *this + a;
     }
 
-    const BigInteger operator *(BigInteger a) const {
-        BigInteger b = *this;
-        if (a.number.size() > b.number.size()) {
-            std::swap(a, b);
+    BigInteger operator -(const BigInteger &a) const {
+        if (sign != a.sign) {
+            return *this + -a;
         }
-        BigInteger res(0);
-        for (int i = 0; i < (int) a.number.size(); i++) {
-            BigInteger cur;
-            for (int j = i; j > 0; j--) {
-                cur.number.push_back(0);
-            }
-            int last = 0;
-            int residue = 0;
-            while (last < (int) b.number.size()) {
-                long long mult = 1LL * b.number[last] * a.number[i] + residue;
-                cur.number.push_back(mult % MOD);
-                residue = mult / MOD;
-                last++;
-            }
-            cur.addResidue((int) cur.number.size(), residue);
-            res += cur;
+        if (*this == a) {
+            return ZERO();
         }
-        while (1 < (int) res.number.size() && res.number.back() == 0) {
-            res.number.pop_back();
+        if (abs(*this) < abs(a)) {
+            return -(a - *this);
+        }
+        BigInteger res = *this;
+        int last = 0;
+        while (last < (int) a.value.size()) {
+            res.value[last] -= a.value[last];
+            if (res.value[last] < 0) {
+                res.value[last] += MOD;
+                res.value[last + 1]--;
+            }
+            last++;
+        }
+        while (1 < (int) res.value.size() && res.value.back() == 0) {
+            res.value.pop_back();
         }
         return res;
     }
 
-    const void operator *=(BigInteger a) {
+    BigInteger operator -(long long num) const {
+        return *this - valueOf(num);
+    }
+
+    void operator -=(const BigInteger &a) {
+        *this = *this - a;
+    }
+
+    BigInteger operator -() const {
+        return {-1 * sign, value};
+    }
+
+    BigInteger operator --() {
+        *this -= ONE();
+        return *this;
+    }
+
+    BigInteger operator ++() {
+        *this += ONE();
+        return *this;
+    }
+
+    BigInteger operator --(int) {
+        *this -= ONE();
+        return *this;
+    }
+
+    BigInteger operator ++(int) {
+        *this += ONE();
+        return *this;
+    }
+
+    BigInteger operator *(const BigInteger &a) const {
+        if (value.size() < a.value.size()) {
+            return a * *this;
+        }
+        BigInteger res = ZERO();
+        for (int i = 0; i < (int) a.value.size(); i++) {
+            BigInteger cur;
+            for (int j = i; j > 0; j--) {
+                cur.value.push_back(0);
+            }
+            int last = 0;
+            int residue = 0;
+            while (last < (int) value.size()) {
+                long long mult = 1LL * value[last] * a.value[i] + residue;
+                cur.value.push_back(mult % MOD);
+                residue = mult / MOD;
+                last++;
+            }
+            cur.addResidue((int) cur.value.size(), residue);
+            res += cur;
+        }
+        while (1 < (int) res.value.size() && res.value.back() == 0) {
+            res.value.pop_back();
+        }
+        res.sign = sign * a.sign;
+        return res;
+    }
+
+    BigInteger operator *(long long num) const {
+        return *this * valueOf(num);
+    }
+
+    void operator *=(const BigInteger &a) {
         *this = *this * a;
     }
 
-    const BigInteger getPow(int pow) const {
-        if (pow == 0) {
-            return BigInteger(1);
+    BigInteger operator /(const BigInteger &b) const {
+        if (b == ZERO()) {
+            throw 1;
         }
-        BigInteger half = getPow(pow / 2);
-        if (pow % 2 == 0) {
-            return half * half;
+        if (abs(*this) < abs(b)) {
+            return ZERO();
         }
-        return half * half * *this;
+        BigInteger a;
+        int last = (int) value.size() - (int) b.value.size();
+        for (int i = last; i < (int) value.size(); i++) {
+            a.value.push_back(value[i]);
+        }
+        if (a < b) {
+            last--;
+            a.value.insert(a.value.begin(), value[last]);
+        }
+        std::vector<int> res;
+        while (true) {
+            int left = 0;
+            int right = MOD + 1;
+            while (left + 1 < right) {
+                int mid = (left + right) >> 1;
+                if (a < b * mid) {
+                    right = mid;
+                } else {
+                    left = mid;
+                }
+            }
+            a -= b * left;
+            res.push_back(left);
+            last--;
+            if (last < 0) {
+                break;
+            }
+            if (a.value.back() == 0) {
+                a.value.pop_back();
+            }
+            a.value.insert(a.value.begin(), value[last]);
+        }
+        std::reverse(res.begin(), res.end());
+        return {sign * b.sign, res};
     }
 
-    void print() {
-        int cur = (int) number.size() - 1;
-        printf("%d", number[cur]);
-        cur--;
-        while (cur >= 0) {
-            printf("%09d", number[cur]);
-            cur--;
+    BigInteger operator /(long long num) const {
+        return *this / valueOf(num);
+    }
+
+    void operator /=(const BigInteger &a) {
+        *this = *this / a;
+    }
+
+    BigInteger operator %(const BigInteger &b) {
+        if (b == ZERO()) {
+            throw 1;
         }
+        if (abs(*this) < abs(b)) {
+            return *this;
+        }
+        BigInteger a;
+        int last = (int) value.size() - (int) b.value.size();
+        for (int i = last; i < (int) value.size(); i++) {
+            a.value.push_back(value[i]);
+        }
+        if (a < b) {
+            last--;
+            a.value.insert(a.value.begin(), value[last]);
+        }
+        while (true) {
+            int left = 0;
+            int right = MOD + 1;
+            while (left + 1 < right) {
+                int mid = (left + right) >> 1;
+                if (a < b * mid) {
+                    right = mid;
+                } else {
+                    left = mid;
+                }
+            }
+            a -= b * left;
+            last--;
+            if (last < 0) {
+                break;
+            }
+            if (a.value.back() == 0) {
+                a.value.pop_back();
+            }
+            a.value.insert(a.value.begin(), value[last]);
+        }
+        return a;
+    }
+
+    BigInteger operator %(long long num) {
+        return *this % valueOf(num);
+    }
+
+    void operator %=(const BigInteger &a) {
+        *this = *this % a;
+    }
+
+    static BigInteger pow(const BigInteger &value, long long ext) {
+        if (ext == 0) {
+            return ONE();
+        }
+        BigInteger half = pow(value, ext >> 1);
+        if ((ext & 1) == 0) {
+            return half * half;
+        }
+        return half * half * value;
+    }
+
+    std::string toString() const {
+        std::string res;
+        if (sign == -1) {
+            res += '-';
+        }
+        for (int i = (int) value.size() - 1; i >= 0; i--) {
+            std::string cur = std::to_string(value[i]);
+            if (i != (int) value.size() - 1) {
+                for (int j = 0; j < CELL_SIZE - (int) cur.length(); j++) {
+                    res += '0';
+                }
+            }
+            res += cur;
+        }
+        return res;
+    }
+
+    int toInt() const {
+        int res = 0;
+        for (int cur : value) {
+            res *= MOD;
+            res += cur;
+        }
+        return sign * res;
+    }
+
+    long long toLong() const {
+        long long res = 0;
+        for (long long cur : value) {
+            res *= MOD;
+            res += cur;
+        }
+        return sign * res;
     }
 };
 
 int main() {
     int n;
     scanf("%d", &n);
-    BigInteger(BigInteger(3).getPow(n * (n - 1) / 2)).print();
+    printf("%s", BigInteger::pow(BigInteger::valueOf(3), n * (n - 1) / 2).toString().c_str());
     return 0;
 }
