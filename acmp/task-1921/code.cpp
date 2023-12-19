4e8ffa5bec0f052a7888da27f65f4cc9569d7125
Subio <subikos@mail.ru>
2023-12-19 19:07:45 +0500

    updated acmp 1921

--- acmp/task-1921/code.cpp
+++ acmp/task-1921/code.cpp
@@ -6,8 +6,8 @@
 int main() {
     int a, b;
     scanf("%d %d", &a, &b);
-    int girl = (a + 1) / (a - b);
-    long long boy = 1LL * girl * b + 1;
-    printf("%lld\n%d", boy, girl);
+    int nGirls = (a + 1) / (a - b);
+    long long nBoys = 1LL * nGirls * b + 1;
+    printf("%lld\n%d", nBoys, nGirls);
     return 0;
 }
