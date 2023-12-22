1ff504a3341995337cdb401c88bc6029ffd66af8
Subio <subikos@mail.ru>
2023-12-22 08:15:29 +0500

    accepted acmp 1901

---
+++ acmp/task-1901/code.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+// Time complexity: O(1)
+// Space complexity: O(1)
+
+int main() {
+    int sizeI, sizeJ;
+    scanf("%d %d", &sizeI, &sizeJ);
+    printf("%lld", 1LL * (sizeI - 1) * (sizeJ - 1));
+    return 0;
+}
