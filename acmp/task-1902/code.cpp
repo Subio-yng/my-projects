cbf73d616a420da9681e26acbb05c48a0d62e3d0
Subio <subikos@mail.ru>
2024-01-04 21:11:04 +0500

    updated acmp 1902

--- acmp/task-1902/code.cpp
+++ acmp/task-1902/code.cpp
@@ -6,7 +6,6 @@
 int main() {
     int a, b;
     scanf("%d %d", &a, &b);
-    double S = abs(-b / 10.0 * 3 * a / 7.0 - b / 14.0 * 3 * a / 5.0) / 2.0;
-    printf("%lf", S);
+    printf("%.4lf", 3 * a * b / 70.0);
     return 0;
 }
