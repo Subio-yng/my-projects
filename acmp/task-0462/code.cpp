5e2ad4df6857b57d0f230cf0b458f697d3121b13
Subio <subikos@mail.ru>
2024-04-04 10:38:34 +0500

    updated amcp 462

--- acmp/task-0462/code.cpp
+++ acmp/task-0462/code.cpp
@@ -6,6 +6,6 @@
 int main() {
     int n;
     scanf("%d", &n);
-    printf("%d", n - (n / 2 + n / 3 - n / 6 + n / 5 - n / 10 - n / 15 + n / 30));
+    printf("%d", (n + 1) / 2 - (n + 3) / 6 - (n + 5) / 10 + (n + 15) / 30);
     return 0;
 }
