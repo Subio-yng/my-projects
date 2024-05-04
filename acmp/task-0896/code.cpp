e67fbd74668827b66fdd0409ee9f2eb43b4e8a3c
Subio <subikos@mail.ru>
2024-05-04 18:07:56 +0500

    updated acmp 456

---
+++ acmp/task-0896/code.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+int getGcd(int a, int b) {
+    while (b > 0) {
+        int tmp = a % b;
+        a = b;
+        b = tmp;
+    }
+    return a;
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a + b);
+    return 0;
+}
