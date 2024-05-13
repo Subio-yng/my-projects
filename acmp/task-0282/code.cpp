24282ccb011a74782bed555898754bf0928848ab
Subio <subikos@mail.ru>
2024-05-13 17:38:40 +0500

    updated acmp 282

--- acmp/task-0282/code.cpp
+++ acmp/task-0282/code.cpp
@@ -1,17 +1,11 @@
 #include <bits/stdc++.h>
 
-// Time complexity: O(sizeI * sizeJ)
-// Space complexity: O(sizeI * sizeJ)
+// Time complexity: O(1)
+// Space complexity: O(1)
 
 int main() {
     int sizeI, sizeJ;
     scanf("%d %d", &sizeI, &sizeJ);
-    std::vector<std::vector<long long>> field(sizeI + 1, std::vector<long long>(sizeJ + 1, 0));
-    for (int i = 1; i <= sizeI; i++) {
-        for (int j = 1; j <= sizeJ; j++) {
-            field[i][j] = field[i - 1][j] + field[i][j - 1] - field[i - 1][j - 1] + i * j;
-        }
-    }
-    printf("%lld", field[sizeI][sizeJ]);
+    printf("%lld", 1LL * sizeI * (sizeI + 1) / 2 * sizeJ * (sizeJ + 1) / 2);
     return 0;
 }
