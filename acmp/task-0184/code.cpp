9d77a65986a2c63414dc5e41b90b9f4d5275c276
Subio <subikos@mail.ru>
2024-05-16 19:18:30 +0500

    updated acmp 184

--- acmp/task-0184/code.cpp
+++ acmp/task-0184/code.cpp
@@ -13,22 +13,19 @@ int getDays(int month) {
     }
 }
 
-int getPrevDays(int n) {
-    int sum = 0;
-    for (int i = 1; i < n; i++) {
-        sum += getDays(i);
-    }
-    return sum;
-}
-
 int main() {
+    const int N_MONTHS = 12;
+    std::vector<int> prevDays(N_MONTHS, 0);
+    for (int i = 1; i < N_MONTHS; i++) {
+        prevDays[i] = prevDays[i - 1] + getDays(i);
+    }
     int n;
     scanf("%d", &n);
     std::vector<int> a(n);
     for (int i = 0; i < n; i++) {
         int day, month, hour, minute;
         scanf("%d.%d. %d:%d", &day, &month, &hour, &minute);
-        a[i] = ((getPrevDays(month) + day - 1) * 8 + hour - 10) * 60 + minute;
+        a[i] = ((prevDays[month - 1] + day - 1) * 8 + hour - 10) * 60 + minute;
     }
     std::sort(a.begin(), a.end());
     int sum = 0;
